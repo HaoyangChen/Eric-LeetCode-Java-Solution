9ce833ffda7adca423930865c9dddf224955d8f7
Haoyang Chen <haoyangchen0901@gmail.com>
2021-01-01 18:19:18 -0800

    Complete Question 76

---
+++ 76.minimum-window-substring.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=76 lang=java
+ *
+ * [76] Minimum Window Substring
+ */
+
+// @lc code=start
+class Solution {
+    public String minWindow(String s, String t) {
+        HashMap<Character, Integer> need = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> window = new HashMap<Character, Integer>();
+        for (char c: t.toCharArray()) need.put(c, need.getOrDefault(c, 0) + 1);
+        int left = 0, right = 0;
+        int valid = 0;
+        int start = 0, len = Integer.MAX_VALUE;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+            while (valid == need.size()) {
+                if (right - left < len) {
+                    start = left;
+                    len = right - left;
+                }
+                char c1 = s.charAt(left);
+                left++;
+                if (need.containsKey(c1)) {
+                    if (window.get(c1).equals(need.get(c1))) {
+                        valid--;
+                    }
+                    window.put(c1, window.getOrDefault(c1, 0) - 1);
+                }
+            }
+        }
+        return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
+    }
+}
+// @lc code=end
+
---
+++ 77.combinations.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=77 lang=java
+ *
+ * [77] Combinations
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        
+    }
+}
+// @lc code=end
+
