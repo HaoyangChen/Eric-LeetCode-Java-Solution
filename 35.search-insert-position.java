81eb88d75aa707e0bc58a7fc5405a0ae3027e3ac
Haoyang Chen <haoyangchen0901@gmail.com>
2021-01-14 00:17:05 -0800

    Completed Question 35

--- 35.search-insert-position.java
+++ 35.search-insert-position.java
@@ -5,9 +5,28 @@
  */
 
 // @lc code=start
+
+// Method 1: Binary Search
+// Time Complexity: O(logN) - where N is the length of the array. Because we shortened the array by half each time, the overall time complexity is O(logN)
+// Space Complexity: O(1) - constant space is used 
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        
+        int len = nums.length;
+        if (len == 0) return 0;
+
+        if (nums[len - 1] < target) return len;
+
+        int left = 0;
+        int right = len - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
     }
 }
 // @lc code=end
