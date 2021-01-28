a426e491d513a48534e23c5251eb164b955de110
Haoyang Chen <haoyangchen0901@gmail.com>
2021-01-27 22:52:10 -0800

    Complete Question 207

--- 207.course-schedule.java
+++ 207.course-schedule.java
@@ -5,37 +5,68 @@
  */
 
 // @lc code=start
+// class Solution {
+//     public boolean canFinish(int numCourses, int[][] prerequisites) {
+//         List<List<Integer>> edges;
+//         int[] indeg;
+//         edges = new ArrayList<List<Integer>>();
+//         for (int i = 0; i < numCourses; ++i) {
+//             edges.add(new ArrayList<Integer>());
+//         }
+//         indeg = new int[numCourses];
+//         for (int[] info: prerequisites) {
+//             edges.get(info[1]).add(info[0]);
+//             ++indeg[info[0]];
+//         }
+//         Queue<Integer> queue = new LinkedList<Integer>();
+//         for (int i = 0; i < numCourses; ++i) {
+//             if (indeg[i] == 0) {
+//                 queue.offer(i);
+//             }
+//         }
+//         int visited = 0;
+//         while (!queue.isEmpty()) {
+//             ++visited;
+//             int u = queue.poll();
+//             for (int v: edges.get(u)) {
+//                 --indeg[v];
+//                 if (indeg[v] == 0) {
+//                     queue.offer(v);
+//                 }
+//             }
+//         }
+//         return visited == numCourses;
+//     }
+// }
+
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> edges;
-        int[] indeg;
-        edges = new ArrayList<List<Integer>>();
-        for (int i = 0; i < numCourses; ++i) {
-            edges.add(new ArrayList<Integer>());
+        int[] inDegree = new int[numCourses];
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        for (int[] prerequisite: prerequisites) {
+            graph.computeIfAbsent(prerequisite[1], k -> new HashSet<>()).add(prerequisite[0]);
+            inDegree[prerequisite[0]]++;
         }
-        indeg = new int[numCourses];
-        for (int[] info: prerequisites) {
-            edges.get(info[1]).add(info[0]);
-            ++indeg[info[0]];
-        }
-        Queue<Integer> queue = new LinkedList<Integer>();
-        for (int i = 0; i < numCourses; ++i) {
-            if (indeg[i] == 0) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < inDegree.length; i++) {
+            if (inDegree[i] == 0) {
                 queue.offer(i);
             }
         }
-        int visited = 0;
         while (!queue.isEmpty()) {
-            ++visited;
-            int u = queue.poll();
-            for (int v: edges.get(u)) {
-                --indeg[v];
-                if (indeg[v] == 0) {
-                    queue.offer(v);
+            int course = queue.poll();
+            numCourses--;
+            if (!graph.containsKey(course)) {
+                continue;
+            }
+            for (int relatedCourse: graph.get(course)) {
+                inDegree[relatedCourse]--;
+                if (inDegree[relatedCourse] == 0) {
+                    queue.offer(relatedCourse);
                 }
             }
         }
-        return visited == numCourses;
+        return numCourses == 0;
     }
 }
 // @lc code=end
