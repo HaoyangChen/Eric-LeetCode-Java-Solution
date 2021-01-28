508f64f7d9d79ab5ef8940fde20288dbfe3abcd1
Haoyang Chen <haoyangchen0901@gmail.com>
2021-01-28 01:06:22 -0800

    Complete Question 103

--- 103.binary-tree-zigzag-level-order-traversal.java
+++ 103.binary-tree-zigzag-level-order-traversal.java
@@ -20,35 +20,69 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+//         List<List<Integer>> ans = new LinkedList<List<Integer>>();
+//         if (root == null) return ans;
+//         Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+//         nodeQueue.offer(root);
+//         boolean isOrderLeft = true;
+
+//         while(!nodeQueue.isEmpty()) {
+//             Deque<Integer> levelList = new LinkedList<Integer>();
+//             int size = nodeQueue.size();
+//             for (int i = 0; i < size; ++i) {
+//                 TreeNode curNode = nodeQueue.poll();
+//                 if (isOrderLeft) {
+//                     levelList.offerLast(curNode.val);
+//                 } else {
+//                     levelList.offerFirst(curNode.val);
+//                 }
+//                 if (curNode.left != null) {
+//                     nodeQueue.offer(curNode.left);
+//                 }
+//                 if (curNode.right != null) {
+//                     nodeQueue.offer(curNode.right);
+//                 }
+//             }
+//             ans.add(new LinkedList<Integer>(levelList));
+//             isOrderLeft = !isOrderLeft;
+//         }
+//         return ans;
+//     }
+// }
+
+
+// shangan solution
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> ans = new LinkedList<List<Integer>>();
-        if (root == null) return ans;
-        Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
-        nodeQueue.offer(root);
-        boolean isOrderLeft = true;
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        boolean leftToRight = false;
+        queue.offer(root);
 
-        while(!nodeQueue.isEmpty()) {
-            Deque<Integer> levelList = new LinkedList<Integer>();
-            int size = nodeQueue.size();
-            for (int i = 0; i < size; ++i) {
-                TreeNode curNode = nodeQueue.poll();
-                if (isOrderLeft) {
-                    levelList.offerLast(curNode.val);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            LinkedList<Integer> level = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node == null) {
+                    continue;
+                }
+                if (leftToRight) {
+                    level.addFirst(node.val);
                 } else {
-                    levelList.offerFirst(curNode.val);
+                    level.add(node.val);
                 }
-                if (curNode.left != null) {
-                    nodeQueue.offer(curNode.left);
-                }
-                if (curNode.right != null) {
-                    nodeQueue.offer(curNode.right);
-                }
+                queue.offer(node.left);
+                queue.offer(node.right);
             }
-            ans.add(new LinkedList<Integer>(levelList));
-            isOrderLeft = !isOrderLeft;
+            if (!level.isEmpty()) {
+                result.add(level);
+            }
+            leftToRight = !leftToRight;
         }
-        return ans;
+        return result;
     }
 }
 // @lc code=end
