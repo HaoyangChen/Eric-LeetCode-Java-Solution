fb4a7957f0ff8a1b970f1e82d62163bb2434ecd7
Haoyang Chen <haoyangchen0901@gmail.com>
2021-02-04 21:12:13 -0800

    Complete Question 162

--- 162.find-peak-element.java
+++ 162.find-peak-element.java
@@ -7,7 +7,20 @@
 // @lc code=start
 class Solution {
     public int findPeakElement(int[] nums) {
-        
+        if (nums.length == 0 || nums == null) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid - 1]) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left] > nums[right] ?  left : right;
     }
 }
 // @lc code=end
