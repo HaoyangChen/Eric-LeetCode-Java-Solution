727645c18afc041ba6dc03f5374ed3fe1d18ecdc
Haoyang Chen <haoyangchen0901@gmail.com>
2021-02-05 23:56:29 -0800

    Complete Question 1

--- 1.two-sum.java
+++ 1.two-sum.java
@@ -5,17 +5,45 @@
  */
 
 // @lc code=start
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         Map<Integer, Integer> map = new HashMap();
+//         for (int i = 0; i < nums.length; i++) {
+//             int complement = target - nums[i];
+//             if (map.containsKey(complement)) {
+//                 return new int[] {map.get(complement), i};
+//             }
+//             map.put(nums[i], i);
+//         }
+//         throw new IllegalArgumentException("No two sum solution");
+//     }
+// }
+
+// Shangan Solution
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap();
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement)) {
-                return new int[] {map.get(complement), i};
-            }
-            map.put(nums[i], i);
+        if(nums == null) {
+            return null;
         }
-        throw new IllegalArgumentException("No two sum solution");
+        Integer[] indexArray = new Integer[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            indexArray[i] = i;
+        }
+        Arrays.sort(indexArray, (i1, i2) -> Integer.compare(nums[i1], nums[i2]));
+        int left = 0;
+        int right = indexArray.length - 1;
+        
+        while (left < right) {
+            if (nums[indexArray[left]] + nums[indexArray[right]] == target) {
+                return new int[]{indexArray[left], indexArray[right]};
+            }
+            if (nums[indexArray[left]] + nums[indexArray[right]] < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return null;
     }
 }
 // @lc code=end
