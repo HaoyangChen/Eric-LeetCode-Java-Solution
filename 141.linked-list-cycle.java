3bdad9ae03aa2636aad30db4a45194a11f7d03a3
Haoyang Chen <haoyangchen0901@gmail.com>
2021-02-08 18:35:27 -0800

    Adjust Question 141

--- 141.linked-list-cycle.java
+++ 141.linked-list-cycle.java
@@ -16,17 +16,33 @@
  *     }
  * }
  */
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+//         ListNode fast, slow;
+//         fast = slow = head;
+//         while (fast != null && fast.next != null) {
+//             fast = fast.next.next;
+//             slow = slow.next;
+//             if (fast == slow) return true;
+//         }
+//         return false;
+//     }
+// }
+
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode fast, slow;
-        fast = slow = head;
+        ListNode slow = head;
+        ListNode fast = head;
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
-            if (fast == slow) return true;
+            if (fast == slow) {
+                return true;
+            }
         }
         return false;
     }
 }
+
 // @lc code=end
 
