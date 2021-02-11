35c4be87fbfb9fdb8c8b3392ed54606c9f16a573
Haoyang Chen <haoyangchen0901@gmail.com>
2021-02-10 17:09:21 -0800

    Complete Question 78

---
+++ 78.subsets.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=78 lang=java
+ *
+ * [78] Subsets
+ */
+
+// @lc code=start
+
+// Shangan Solution
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        result.add(path);
+        helper(nums, path, result, 0);
+        return result;
+    }
+
+    private void helper(int[] nums, List<Integer> path, List<List<Integer>> result, int start) {
+        for (int i = start; i < nums.length; i++) {
+            if (path.contains(nums[i])) {
+                continue;
+            }
+            path.add(nums[i]);
+            result.add(new ArrayList<>(path));
+            helper(nums, path, result, start + i);
+            path.remove(path.size() - 1);
+        }
+    }
+}
+// @lc code=end
+
