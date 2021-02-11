8eb2c48ced81635411453722158393408d9916f9
Haoyang Chen <haoyangchen0901@gmail.com>
2021-02-10 22:28:01 -0800

    Complete Question 40

---
+++ 40.combination-sum-ii.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=40 lang=java
+ *
+ * [40] Combination Sum II
+ */
+
+// @lc code=start
+
+// Shangan Solution
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        Arrays.sort(candidates);
+        dfs(candidates, target, result, path, 0);
+        return result;
+    }
+
+    private void dfs(int[] candidates, int target, List<List<Integer>> result, List<Integer> path, int index) {
+        if (target == 0) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = index; i < candidates.length && candidates[i] <= target; i++) {
+            if (i > index && candidates[i] == candidates[i - 1]) continue;
+            path.add(candidates[i]);
+            dfs(candidates, target - candidates[i], result, path, i + 1);
+            path.remove(path.size() - 1);
+        }
+    }
+}
+// @lc code=end
+
