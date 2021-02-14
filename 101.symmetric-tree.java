db17d73f246c34c051271e73fb531164a52a210f
Haoyang Chen <haoyangchen0901@gmail.com>
2021-02-14 00:04:26 -0800

    Add More Methods to Question 101

--- 101.symmetric-tree.java
+++ 101.symmetric-tree.java
@@ -21,28 +21,66 @@
  * }
  */
 
+// Shangan Solution Method: stack
+// class Solution {
+//     public boolean isSymmetric(TreeNode root) {
+//         if (root == null) return true;
+//         Stack<TreeNode> stack = new Stack<>();
+//         stack.push(root.left);
+//         stack.push(root.right);
+//         while (!stack.isEmpty()) {
+//             TreeNode n1 = stack.pop();
+//             TreeNode n2 = stack.pop();
+//             if (n1 == null && n2 == null) {
+//                 continue;
+//             }
+//             if (n1 == null || n2 == null || n1.val != n2.val) {
+//                 return false;
+//             }
+//             stack.push(n1.left);
+//             stack.push(n2.right);
+//             stack.push(n1.right);
+//             stack.push(n2.left);
+//         }
+//         return true;
+//     }
+// }
+
 // Shangan Solution
+// Time Complexity: O(n) - because we traverse the entire input tree once, the total run time is                            O(n), where n is the total number of nodes in the tree
+// Space Complexity: O(n) - The number of recursive calls is bound by the height of the tree. In 
+//                          the worst case, the tree is linear and height is in O(n). Therefore, 
+//                          the space complexity due to recursive calls on stack is O(n) in the                               worst case
+// class Solution {
+//     public boolean isSymmetric(TreeNode root) {
+//         return root == null || helper(root.left, root.right);
+//     }
+    
+//     private boolean helper(TreeNode left, TreeNode right) {
+//         if (left == null || right == null) {
+//             return left == right;
+//         }
+//         if (left.val != right.val) {
+//             return false;
+//         }
+//         return helper(left.left, right.right) && helper(left.right, right.left);
+//     }
+// }
+
+// leetcode solution: recursive
+// Time Complexity: O(n) - because we traverse the entire input tree once, the total run time is                            O(n), where n is the total number of nodes in the tree
+// Space Complexity: O(n) - The number of recursive calls is bound by the height of the tree. In 
+//                          the worst case, the tree is linear and height is in O(n). Therefore, 
+//                          the space complexity due to recursive calls on stack is O(n) in the //                          worst case
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) return true;
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root.left);
-        stack.push(root.right);
-        while (!stack.isEmpty()) {
-            TreeNode n1 = stack.pop();
-            TreeNode n2 = stack.pop();
-            if (n1 == null && n2 == null) {
-                continue;
-            }
-            if (n1 == null || n2 == null || n1.val != n2.val) {
-                return false;
-            }
-            stack.push(n1.left);
-            stack.push(n2.right);
-            stack.push(n1.right);
-            stack.push(n2.left);
-        }
-        return true;
+        return isMirror(root, root);
+    }
+    
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+        return (t1.val == t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
     }
 }
 // @lc code=end
