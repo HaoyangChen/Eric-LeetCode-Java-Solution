98374f6a3d680a21430b40208b9bd06fa40c5eb0
Haoyang Chen <haoyangchen0901@gmail.com>
2021-02-17 19:00:57 -0800

    Add More Method to Question 797

--- 797.all-paths-from-source-to-target.java
+++ 797.all-paths-from-source-to-target.java
@@ -5,34 +5,62 @@
  */
 
 // @lc code=start
-
 // LeetCode Solution
+// class Solution {
+//     private int target;
+//     private int[][] graph;
+//     private List<List<Integer>> results;
+    
+//     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+//         this.target = graph.length - 1;
+//         this.graph = graph;
+//         this.results = new ArrayList<List<Integer>>();
+//         LinkedList<Integer> path = new LinkedList<Integer>();
+//         path.addLast(0);
+//         this.backtrack(0, path);
+//         return results;
+//     }
+    
+//     protected void backtrack(int currNode, LinkedList<Integer> path) {
+//         if (currNode == this.target) {
+//             this.results.add(new ArrayList<Integer>(path));
+//             return;
+//         }
+//         for (int nextNode: this.graph[currNode]) {
+//             path.add(nextNode);
+//             backtrack(nextNode, path);
+//             path.removeLast();
+//         }
+//     }
+// }
+
+
+// Shangan Solution
 class Solution {
-    private int target;
-    private int[][] graph;
-    private List<List<Integer>> results;
+    private List<List<Integer>> result;
     
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        this.target = graph.length - 1;
-        this.graph = graph;
-        this.results = new ArrayList<List<Integer>>();
-        LinkedList<Integer> path = new LinkedList<Integer>();
-        path.addLast(0);
-        this.backtrack(0, path);
-        return results;
+        result = new ArrayList<>();
+        if (graph == null || graph.length == 0) {
+            return result;
+        }
+        List<Integer> path = new ArrayList<>();
+        path.add(0);
+        dfs(graph, 0, path);
+        return result;
     }
     
-    protected void backtrack(int currNode, LinkedList<Integer> path) {
-        if (currNode == this.target) {
-            this.results.add(new ArrayList<Integer>(path));
+    private void dfs(int[][] graph, int index, List<Integer> path) {
+        if (index == graph.length - 1) {
+            result.add(new ArrayList<>(path));
             return;
         }
-        for (int nextNode: this.graph[currNode]) {
-            path.add(nextNode);
-            backtrack(nextNode, path);
-            path.removeLast();
+        for (int neighbor: graph[index]) {
+            path.add(neighbor);
+            dfs(graph, neighbor, path);
+            path.remove(path.size() - 1);
         }
-    }
+    }  
 }
 // @lc code=end
 
