e68a79c9f466a2660013509ad112ef60fd05b827
Haoyang Chen <haoyangchen0901@gmail.com>
2021-02-21 17:58:52 -0800

    Complete Question 513

--- 513.find-bottom-left-tree-value.java
+++ 513.find-bottom-left-tree-value.java
@@ -20,28 +20,52 @@
  *     }
  * }
  */
-class Solution {
-    public int findBottomLeftValue(TreeNode root) {
-        int result = 0;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
+// class Solution {
+//     public int findBottomLeftValue(TreeNode root) {
+//         int result = 0;
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.offer(root);
 
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                if (i == size - 1) {
-                    result = node.val;
-                }
-                if (node.right != null) {
-                    queue.offer(node.right);
-                }
-                if (node.left != null) {
-                    queue.offer(node.left);
-                }
-            }
-        }
+//         while (!queue.isEmpty()) {
+//             int size = queue.size();
+//             for (int i = 0; i < size; i++) {
+//                 TreeNode node = queue.poll();
+//                 if (i == size - 1) {
+//                     result = node.val;
+//                 }
+//                 if (node.right != null) {
+//                     queue.offer(node.right);
+//                 }
+//                 if (node.left != null) {
+//                     queue.offer(node.left);
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// }
+
+class Solution {
+    private int result;
+    private int maxDepth;
+    public int findBottomLeftValue(TreeNode root) {
+        result = 0;
+        maxDepth = 0;
+        helper(root, 1);
         return result;
+    }
+    
+    private void helper(TreeNode node, int depth) {
+        if (node == null) {
+            return;
+        }
+        if (maxDepth < depth) {
+            result = node.val;
+            maxDepth = depth;
+        }
+        depth++;
+        helper(node.left, depth);
+        helper(node.right, depth);
     }
 }
 // @lc code=end
