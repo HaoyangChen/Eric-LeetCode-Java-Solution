def6ec7d9b231c5a8c0fdcb1bed134f130d24c14
Haoyang Chen <haoyangchen0901@gmail.com>
2021-02-28 22:54:11 -0800

    Adjust Question 226

--- 226.invert-binary-tree.java
+++ 226.invert-binary-tree.java
@@ -20,17 +20,29 @@
  *     }
  * }
  */
+// class Solution {
+//     public TreeNode invertTree(TreeNode root) {
+//         if (root == null) return null;
+//         TreeNode temp = root.left;
+//         root.left = root.right;
+//         root.right = temp;
+
+//         invertTree(root.left);
+//         invertTree(root.right);
+//         return root;
+//     }
+// }
+
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
-        TreeNode temp = root.left;
-        root.left = root.right;
-        root.right = temp;
-
-        invertTree(root.left);
-        invertTree(root.right);
+        TreeNode right = invertTree(root.right);
+        TreeNode left = invertTree(root.left);
+        root.left = right;
+        root.right = left;
         return root;
     }
 }
+
 // @lc code=end
 
