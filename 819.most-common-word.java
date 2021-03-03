e4a5510b1b6279beb2e5897a1f73fc642ee74cfc
Haoyang Chen <haoyangchen0901@gmail.com>
2021-03-02 23:40:12 -0800

    Add Additional Solution to Question 819

--- 819.most-common-word.java
+++ 819.most-common-word.java
@@ -10,22 +10,67 @@
 // Let N be the number of characters in the input string and M be the number of characters in the banned list.
 // Time Complexity: O(N + M)
 // Space Complexity: O(N + M)
+// class Solution {
+//     public String mostCommonWord(String paragraph, String[] banned) {
+//         String normalizedStr = paragraph.replaceAll("[^a-zA-Z0-9]", " ").toLowerCase();
+//         String[] words = normalizedStr.split("\\s+");
+//         Set<String> bannedWords = new HashSet();
+//         for (String word: banned) {
+//             bannedWords.add(word);
+//         }
+//         Map<String, Integer> wordCount = new HashMap<>();
+//         for (String word: words) {
+//             if (!bannedWords.contains(word)) {
+//                 wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+//             }
+//         }
+//         return Collections.max(wordCount.entrySet(), Map.Entry.comparingByValue()).getKey();
+//     }
+// }
+
+// Character Processing in One-Pass
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
-        String normalizedStr = paragraph.replaceAll("[^a-zA-Z0-9]", " ").toLowerCase();
-        String[] words = normalizedStr.split("\\s+");
         Set<String> bannedWords = new HashSet();
         for (String word: banned) {
             bannedWords.add(word);
         }
-        Map<String, Integer> wordCount = new HashMap<>();
-        for (String word: words) {
-            if (!bannedWords.contains(word)) {
-                wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+
+        String ans = "";
+        int maxCount = 0;
+        Map<String, Integer> wordCount = new HashMap();
+        StringBuilder wordBuffer = new StringBuilder();
+        char[] chars = paragraph.toCharArray();
+
+        for (int p = 0; p < chars.length; p++) {
+            char currChar = chars[p];
+            // 1). consume the character in a word
+            if (Character.isLetter(currChar)) {
+                wordBuffer.append(Character.toLowerCase(currChar));
+                if (p != chars.length - 1) {
+                    // skip the rest of processing
+                    continue;
+                }
             }
+            // 2). at the end of one word or at the end of paragraph
+            if (wordBuffer.length() > 0) {
+                String word = wordBuffer.toString();
+                if (!bannedWords.contains(word)) {
+                    int newCount = wordCount.getOrDefault(word, 0) + 1;
+                    wordCount.put(word, newCount);
+                    // identify the maximum count while updating the wordCount table
+                    if (newCount > maxCount) {
+                        ans = word;
+                        maxCount = newCount;
+                    }
+                }
+            }
+            // reset the buffer for the next word
+            wordBuffer = new StringBuilder();
         }
-        return Collections.max(wordCount.entrySet(), Map.Entry.comparingByValue()).getKey();
+        return ans;
     }
 }
+
 // @lc code=end
 
