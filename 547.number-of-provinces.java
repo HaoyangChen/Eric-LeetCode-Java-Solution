bd60277af486cd44d3363936ef43a084f2da5a9a
Haoyang Chen <haoyangchen0901@gmail.com>
2021-03-14 23:14:34 -0700

    Complete Question 547

---
+++ 547.number-of-provinces.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=547 lang=java
+ *
+ * [547] Number of Provinces
+ */
+
+// @lc code=start
+
+// Method 1: Using Depth First Search
+// Time Complexity: O(n^2) The complete matrix of size n^2 is traversed
+// Space Complexity: O(n) - visited array of size n is used
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int[] visited = new int[isConnected.length];
+        int count = 0;
+        for (int i = 0; i < isConnected.length; i++) {
+            if (visited[i] == 0) {
+                dfs(isConnected, visited, i);
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public void dfs(int[][] isConnected, int[] visited, int i) {
+        for (int j = 0; j < isConnected.length; j++) {
+            if (isConnected[i][j] == 1 && visited[j] == 0) {
+                visited[j] = 1;
+                dfs(isConnected, visited, j);
+            }
+        }
+    }
+}
+// @lc code=end
+
