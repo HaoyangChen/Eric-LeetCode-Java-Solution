75bf3633a76608b1a17078cb5b1d313b13ea8b49
Haoyang Chen <haoyangchen0901@gmail.com>
2021-03-14 23:51:48 -0700

    Add BFS to Problem 547

--- 547.number-of-provinces.java
+++ 547.number-of-provinces.java
@@ -9,27 +9,55 @@
 // Method 1: Using Depth First Search
 // Time Complexity: O(n^2) The complete matrix of size n^2 is traversed
 // Space Complexity: O(n) - visited array of size n is used
+// class Solution {
+//     public int findCircleNum(int[][] isConnected) {
+//         int[] visited = new int[isConnected.length];
+//         int count = 0;
+//         for (int i = 0; i < isConnected.length; i++) {
+//             if (visited[i] == 0) {
+//                 dfs(isConnected, visited, i);
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+    
+//     public void dfs(int[][] isConnected, int[] visited, int i) {
+//         for (int j = 0; j < isConnected.length; j++) {
+//             if (isConnected[i][j] == 1 && visited[j] == 0) {
+//                 visited[j] = 1;
+//                 dfs(isConnected, visited, j);
+//             }
+//         }
+//     }
+// }
+
+// Method 2: Using Breadth First Search
+// Time Complexity: O(n^2) The complete matrix of size n^2 is traversed
+// Space Complexity: O(n) - A queue and visited array of size n is used
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int[] visited = new int[isConnected.length];
         int count = 0;
+        Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < isConnected.length; i++) {
             if (visited[i] == 0) {
-                dfs(isConnected, visited, i);
+                queue.add(i);
+                while (!queue.isEmpty()) {
+                    int s = queue.remove();
+                    visited[s] = 1;
+                    for (int j = 0; j < isConnected.length; j++) {
+                        if (isConnected[s][j] == 1 && visited[j] == 0) {
+                            queue.add(j);
+                        }
+                    }
+                }
                 count++;
             }
         }
         return count;
     }
-    
-    public void dfs(int[][] isConnected, int[] visited, int i) {
-        for (int j = 0; j < isConnected.length; j++) {
-            if (isConnected[i][j] == 1 && visited[j] == 0) {
-                visited[j] = 1;
-                dfs(isConnected, visited, j);
-            }
-        }
-    }
 }
+
 // @lc code=end
 
