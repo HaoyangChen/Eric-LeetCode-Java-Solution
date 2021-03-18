2b4e8c6b3af2af6c9ea46efe54f12ab4d2ad0928
Haoyang Chen <haoyangchen0901@gmail.com>
2021-03-17 23:53:59 -0700

    Start Problem 886

---
+++ 886.possible-bipartition.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=886 lang=java
+ *
+ * [886] Possible Bipartition
+ */
+
+// @lc code=start
+class Solution {
+    public boolean possibleBipartition(int N, int[][] dislikes) {
+        
+    }
+}
+// @lc code=end
+
