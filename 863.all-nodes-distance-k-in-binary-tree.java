d7c09e25e3981932a0706e499aca8ee784d61c95
Haoyang Chen <haoyangchen0901@gmail.com>
2021-03-21 01:02:34 -0700

    Complete Question 863

---
+++ 863.all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=863 lang=java
+ *
+ * [863] All Nodes Distance K in Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// Time Complexity: O(N), where N is the number of nodes in the tree
+// Space Complexity: O(N)
+class Solution {
+    Map<TreeNode, TreeNode> parent;
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        parent = new HashMap();
+        dfs(root, null);
+        
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(null);
+        queue.add(target);
+        
+        Set<TreeNode> seen = new HashSet();
+        seen.add(target);
+        seen.add(null);
+        
+        int dist = 0;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node == null) {
+                if (dist == K) {
+                    List<Integer> ans = new ArrayList();
+                    for (TreeNode n: queue) {
+                        ans.add(n.val);
+                    }
+                    return ans;
+                }
+                queue.offer(null);
+                dist++;
+            } else {
+                if (!seen.contains(node.left)) {
+                    seen.add(node.left);
+                    queue.offer(node.left);
+                }
+                if (!seen.contains(node.right)) {
+                    seen.add(node.right);
+                    queue.offer(node.right);
+                }
+                TreeNode par = parent.get(node);
+                if (!seen.contains(par)) {
+                    seen.add(par);
+                    queue.offer(par);
+                }
+            }
+        }
+        return new ArrayList<Integer>();
+    }
+    
+    public void dfs(TreeNode node, TreeNode par) {
+        if (node != null) {
+            parent.put(node, par);
+            dfs(node.left, node);
+            dfs(node.right, node);
+        }
+    }
+}
+// @lc code=end
+
