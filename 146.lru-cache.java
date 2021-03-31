268f59482a10d78b1263932ad1c0079fd3877bb3
Haoyang Chen <haoyangchen0901@gmail.com>
2021-03-30 23:55:49 -0700

    Add Second Implementation to Question 146

--- 146.lru-cache.java
+++ 146.lru-cache.java
@@ -7,27 +7,114 @@
 // @lc code=start
 // Time complexity : O(1) both for put and get since all operations with ordered dictionary : (get/containsKey/put/remove) are done in a constant time.
 // Space Complexity: O(capacity) since the sapce is used only for an ordered dictionary with at most capacity + 1
-class LRUCache extends LinkedHashMap<Integer, Integer>{
-    private int capacity;
+// class LRUCache extends LinkedHashMap<Integer, Integer>{
+//     private int capacity;
     
+//     public LRUCache(int capacity) {
+//         super(capacity, 0.75F, true);
+//         this.capacity = capacity;
+//     }
+    
+//     public int get(int key) {
+//         return super.getOrDefault(key, -1);
+//     }
+    
+//     public void put(int key, int value) {
+//         super.put(key, value);
+//     }
+//     @Override
+//     protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+//         return size() > capacity;
+//     }
+// }
+
+// Time Complexity: O(1) both for put and get
+// Space Complexity: O(capacity) since the space is used only for a hashmap and double linked list with at most capacity + 1 elements
+class LRUCache {
+
+    class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode prev;
+        DLinkedNode next;
+    }
+
+    private void addNode(DLinkedNode node) {
+        // always add the new node right after head
+        node.prev = head;
+        node.next = head.next;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(DLinkedNode node) {
+        // Remove an existing node from the linked list
+        DLinkedNode prev = node.prev;
+        DLinkedNode next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    private void moveToHead(DLinkedNode node) {
+        // move a node in between to the head
+        removeNode(node);
+        addNode(node);
+    }
+
+    private DLinkedNode popTail() {
+        // pop the current tail
+        DLinkedNode res = tail.prev;
+        removeNode(res);
+        return res;
+    }
+
+    private Map<Integer, DLinkedNode> cache = new HashMap<>();
+    private int size;
+    private int capacity;
+    private DLinkedNode head, tail;
+
+
     public LRUCache(int capacity) {
-        super(capacity, 0.75F, true);
+        this.size = 0;
         this.capacity = capacity;
+
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head.next = tail;
+        tail.prev = head;
     }
     
     public int get(int key) {
-        return super.getOrDefault(key, -1);
+        DLinkedNode node = cache.get(key);
+        if (node == null) return -1;
+        // move the accessed node to the head
+        moveToHead(node);
+        return node.value;
     }
     
     public void put(int key, int value) {
-        super.put(key, value);
-    }
-    @Override
-    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-        return size() > capacity;
+        DLinkedNode node = cache.get(key);
+
+        if (node == null) {
+            DLinkedNode newNode = new DLinkedNode();
+            newNode.key = key;
+            newNode.value = value;
+
+            cache.put(key, newNode);
+            addNode(newNode);
+            ++size;
+            if (size > capacity) {
+                DLinkedNode tail = popTail();
+                cache.remove(tail.key);
+                --size;
+            } 
+        } else {
+            node.value = value;
+            moveToHead(node);
+        }
     }
 }
-
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache obj = new LRUCache(capacity);
