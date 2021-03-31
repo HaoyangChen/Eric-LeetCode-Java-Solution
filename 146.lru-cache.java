9200da680626e7ae628b9bc4621f759755a4f61e
Haoyang Chen <haoyangchen0901@gmail.com>
2021-03-30 21:58:32 -0700

    Complete Question 146

--- 146.lru-cache.java
+++ 146.lru-cache.java
@@ -5,18 +5,26 @@
  */
 
 // @lc code=start
-class LRUCache {
-
+// Time complexity : O(1) both for put and get since all operations with ordered dictionary : (get/containsKey/put/remove) are done in a constant time.
+// Space Complexity: O(capacity) since the sapce is used only for an ordered dictionary with at most capacity + 1
+class LRUCache extends LinkedHashMap<Integer, Integer>{
+    private int capacity;
+    
     public LRUCache(int capacity) {
-        
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
     }
     
     public int get(int key) {
-        
+        return super.getOrDefault(key, -1);
     }
     
     public void put(int key, int value) {
-        
+        super.put(key, value);
+    }
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
     }
 }
 
---
+++ 684.redundant-connection.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=684 lang=java
+ *
+ * [684] Redundant Connection
+ */
+
+// @lc code=start
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        
+    }
+}
+// @lc code=end
+
