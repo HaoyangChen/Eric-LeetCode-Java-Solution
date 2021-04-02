e337b75146781121a2389715cac58c34b87e3cd1
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-01 22:41:58 -0700

    Add Heap Implementation to Question 973

--- 973.k-closest-points-to-origin.java
+++ 973.k-closest-points-to-origin.java
@@ -7,30 +7,58 @@
 // @lc code=start
 
 // Method 1: Sort
-// Time Complexity: O(NlogN), where NN is the length of points.
+// Time Complexity: O(NlogN), where N is the length of points.
 // Space Complexity: O(N).
+// class Solution {
+//     public int[][] kClosest(int[][] points, int k) {
+//         int N = points.length;
+//         int[] dists = new int[N];
+//         for (int i = 0; i < N; ++i) {
+//             dists[i] = dist(points[i]);
+//         }
+//         Arrays.sort(dists);
+//         int distK = dists[k - 1];
+        
+//         int[][] ans = new int[k][2];
+//         int t = 0;
+//         for (int i = 0; i < N; ++i) {
+//             if (dist(points[i]) <= distK) ans[t++] = points[i];
+//         }
+//         return ans;
+//     }
+    
+//     private int dist(int[] point) {
+//         return point[0] * point[0] + point[1] * point[1];
+//     }
+// }
+
+// Method 2: Heap
+// Time Complexity: O(nlogK) where n is the length of points
+// Space Complexity: O(K) - because we have at most K points in the heap
 class Solution {
-    public int[][] kClosest(int[][] points, int k) {
-        int N = points.length;
-        int[] dists = new int[N];
-        for (int i = 0; i < N; ++i) {
-            dists[i] = dist(points[i]);
+    public int[][] kClosest(int[][] points, int K) {
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            public int compare(int[] array1, int[] array2) {
+                return array2[0] - array1[0];
+            }
+        });
+
+        for (int i = 0; i < K; i++) {
+            pq.offer(new int[]{points[i][0] * points[i][0] + points[i][1] * points[i][1], i});
         }
-        
-        Arrays.sort(dists);
-        int distK = dists[k - 1];
-        
-        int[][] ans = new int[k][2];
-        int t = 0;
-        for (int i = 0; i < N; ++i) {
-            if (dist(points[i]) <= distK) ans[t++] = points[i];
+        int n = points.length;
+        for (int i = K; i < n; ++i) {
+            int dist = points[i][0] * points[i][0] + points[i][1] * points[i][1];
+            if (dist < pq.peek()[0]) {
+                pq.poll();
+                pq.offer(new int[]{dist, i});
+            }
+        }
+        int[][] ans = new int[K][2];
+        for (int i = 0; i < K; i++) {
+            ans[i] = points[pq.poll()[1]];
         }
         return ans;
-        
-    }
-    
-    private int dist(int[] point) {
-        return point[0] * point[0] + point[1] * point[1];
     }
 }
 // @lc code=end
