3e6326d5989ebff5a12f1f7f768726a0eba4df9f
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-03 22:39:18 -0700

    Complete Iterative Implementation of Question 145

--- 145.binary-tree-postorder-traversal.java
+++ 145.binary-tree-postorder-traversal.java
@@ -24,23 +24,75 @@
  // Method 1: Recursive Postorder Traversal
  // Time Complexity: O(N) - where N is the number of nodes. We visit each node exactly once, thus the time complexity is O(N)
  // Space Complexity: O(N) -  Up to O(H) to keep the recursion stack, where H is the tree height. H is the tree height. In the worst case of the skewed tree H = N.
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         postorderTraversal(root, ans);
+//         return ans;
+//     }
+    
+//     private void postorderTraversal(TreeNode root, List<Integer> ans) {
+//         if (root == null) {
+//             return;
+//         }
+//         postorderTraversal(root.left, ans);
+//         postorderTraversal(root.right, ans);
+//         ans.add(root.val);
+//     }
+// }
+
+// Method 2: Iterative Postorder Traversal: Tweak the Order of the Output
+// Time Complexity: O(N) - where N is the number of nodes. We visit each node exactly once, thus the time complexity is O(N)
+// Space Complexity: O(N) -  Up to O(H) to keep the recursion stack, where H is the tree height. H is the tree height. In the worst case of the skewed tree H = N.
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         LinkedList<Integer> output = new LinkedList();
+//         Deque<TreeNode> stack = new ArrayDeque();
+//         if (root == null) return output;
+
+//         stack.push(root);
+//         while (!stack.isEmpty()) {
+//             root = stack.pop();
+//             output.addFirst(root.val);
+//             if (root.left != null) stack.push(root.left);
+//             if (root.right != null) stack.push(root.right);
+//         }
+//         return output;
+//     }
+// }
+
+// Method 3: Iterative Postorder Traversal
+// Time Complexity: O(N) - where N is the number of nodes. We visit each node exactly once, thus the time complexity is O(N)
+// Space Complexity: O(N) -  Up to O(H) to keep the recursion stack, where H is the tree height. H is the tree height. In the worst case of the skewed tree H = N.
+
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        postorderTraversal(root, ans);
-        return ans;
-    }
-    
-    private void postorderTraversal(TreeNode root, List<Integer> ans) {
-        if (root == null) {
-            return;
+        List<Integer> output = new ArrayList();
+        Deque<TreeNode> stack = new ArrayDeque();
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                if (root.right != null) {
+                    stack.push(root.right);
+                }
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            
+            // if the right subtree is not yet processed
+            if (!stack.isEmpty() && root.right == stack.peek()) {
+                stack.pop();
+                stack.push(root);
+                root = root.right;  
+            // if we're on the leftmost leaf  
+            } else {
+                output.add(root.val);
+                root = null;     
+            }   
         }
-        postorderTraversal(root.left, ans);
-        postorderTraversal(root.right, ans);
-        ans.add(root.val);
+        return output;
     }
 }
-
-
 // @lc code=end
 
