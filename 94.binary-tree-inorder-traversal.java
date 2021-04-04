bd0fc9ef06ba56e0a91b5bb2028a1e5a3f5dbf76
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-03 17:39:32 -0700

    Add Iteration Implementation to Question 94

--- 94.binary-tree-inorder-traversal.java
+++ 94.binary-tree-inorder-traversal.java
@@ -20,24 +20,50 @@
  *     }
  * }
  */
+
+ // Method 1: Recursive Approach
+ // Time Complexity: O(n)
+ // Space Complexity: O(n)
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> res = new ArrayList<>();
+//         helper(root, res);
+//         return res;
+//     }
+
+//     public void helper(TreeNode root, List<Integer> res) {
+//         if (root != null) {
+//             if (root.left != null) {
+//                 helper(root.left, res);
+//             }
+//             res.add(root.val);
+//             if (root.right != null) {
+//                 helper(root.right, res);
+//             }
+//         }
+//     }
+// }
+
+// Method 2: Iterating Method using Stack
+// Time Complexity: O(n)
+// Space Complexity: O(n)
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
         List<Integer> res = new ArrayList<>();
-        helper(root, res);
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
         return res;
     }
-
-    public void helper(TreeNode root, List<Integer> res) {
-        if (root != null) {
-            if (root.left != null) {
-                helper(root.left, res);
-            }
-            res.add(root.val);
-            if (root.right != null) {
-                helper(root.right, res);
-            }
-        }
-    }
 }
+
 // @lc code=end
 
