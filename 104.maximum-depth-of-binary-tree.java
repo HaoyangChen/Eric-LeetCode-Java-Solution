0aec82f2da61c699b1d23d333a2767d66d204ac4
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-04 19:50:04 -0700

    Add Iteration Implementation to Question 104

--- 104.maximum-depth-of-binary-tree.java
+++ 104.maximum-depth-of-binary-tree.java
@@ -21,18 +21,46 @@
  * }
  */
 
- // shangan solution
- // Time Complexity: We visit each node exactly once, thus the time complexity is O(N), where N is the number of nodes
- // Space Complexity: In the worst case, the tree is completely unbalanced, e.g. each node has only left child node, the recursion call would occur N times (the height of the tree), therefore the storage to keep the call stack would be O(N). 
- //                   But in the best case (the tree is completely balanced), the height of the tree would be log(N). Therefore, the space complexity in this case would be O(log(N)).
+// Method 1: Recursion
+// Time Complexity: We visit each node exactly once, thus the time complexity is O(N), where N is the number of nodes
+// Space Complexity: In the worst case, the tree is completely unbalanced, e.g. each node has only left child node, the recursion call would occur N times (the height of the tree), therefore the storage to keep the call stack would be O(N). 
+//                   But in the best case (the tree is completely balanced), the height of the tree would be log(N). Therefore, the space complexity in this case would be O(log(N)).
+// class Solution {
+//     public int maxDepth(TreeNode root) {
+//         if (root == null) {
+//             return 0;
+//         }
+//         int maxLeft = maxDepth(root.left);
+//         int maxRight = maxDepth(root.right);
+//         return Math.max(maxLeft, maxRight) + 1;
+//     }
+// }
+
+// Method 2: Iteration
+// Time Complexity: O(N)
+// Space Complexity: In the worst case, the tree is completely unbalanced
 class Solution {
     public int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> depths = new LinkedList<>();
+        if (root == null) return 0;
+
+        stack.add(root);
+        depths.add(1);
+
+        int depth = 0, current_depth = 0;
+        while (!stack.isEmpty()) {
+            root = stack.pollLast();
+            current_depth = depths.pollLast();
+            if (root != null) {
+                depth = Math.max(depth, current_depth);
+                stack.add(root.left);
+                stack.add(root.right);
+                depths.add(current_depth + 1);
+                depths.add(current_depth + 1);
+            }
         }
-        int maxLeft = maxDepth(root.left);
-        int maxRight = maxDepth(root.right);
-        return Math.max(maxLeft, maxRight) + 1;
+        return depth;
     }
 }
 // @lc code=end
