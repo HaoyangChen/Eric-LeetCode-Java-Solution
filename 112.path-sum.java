0db6f21ea49358039768784ee20eddd5d90ad864
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-05 20:45:33 -0700

    Add Iteration Implementation to Question 112

--- 112.path-sum.java
+++ 112.path-sum.java
@@ -26,16 +26,52 @@
 // Space Complexity: in the worst case, the tree is completely unbalanced, each node has only one child node, the                              recursion call would occur N times (the height of the tree). Therefore, the storage to keep the call
 //                   stack would be O(N). But in the best case (the tree is completely balanced), the height of the tree 
 //                   would be log(N). Therefore, the time complexity in this case would be O(log(N))
+// class Solution {
+//     public boolean hasPathSum(TreeNode root, int targetSum) {
+//         if (root == null) {
+//             return false;
+//         }
+//         if (root.left == null && root.right == null) {
+//             return targetSum == root.val;
+//         }
+//         int remain = targetSum - root.val;
+//         return hasPathSum(root.left, remain) || hasPathSum(root.right, remain);
+//     }
+// }
+
+// Method 2: Iteration
+// Time Complexity: the same as recursion method --  we visit each node exactly once, thus the time complexity is O(N), where N is the number of nodes
+// Space Complexity: in the worst case, the tree is completely unbalanced, each node has only one child node, the                              recursion call would occur N times (the height of the tree). Therefore, the storage to keep the call
+//                   stack would be O(N). But in the best case (the tree is completely balanced), the height of the tree 
+//                   would be log(N). Therefore, the time complexity in this case would be O(log(N))
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null) {
-            return false;
+                return false;
         }
-        if (root.left == null && root.right == null) {
-            return targetSum == root.val;
+        LinkedList<TreeNode> node_stack = new LinkedList<>();
+        LinkedList<Integer> sum_stack = new LinkedList<>();
+        node_stack.add(root);
+        sum_stack.add(targetSum - root.val);
+
+        TreeNode node;
+        int curr_sum;
+        while (!node_stack.isEmpty()) {
+            node = node_stack.pollLast();
+            curr_sum = sum_stack.pollLast();
+            if ((node.left == null) && (node.right == null) && (curr_sum == 0)) {
+                return true;
+            }
+            if (node.right != null) {
+                node_stack.add(node.right);
+                sum_stack.add(curr_sum - node.right.val);
+            }
+            if (node.left != null) {
+                node_stack.add(node.left);
+                sum_stack.add(curr_sum - node.left.val);
+            }
         }
-        int remain = targetSum - root.val;
-        return hasPathSum(root.left, remain) || hasPathSum(root.right, remain);
+        return false;
     }
 }
 // @lc code=end
