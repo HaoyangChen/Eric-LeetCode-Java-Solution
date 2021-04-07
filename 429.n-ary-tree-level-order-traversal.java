c492aaac5a983200449624e7cda41252069243c8
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-06 22:44:05 -0700

    Add Simplified BFS Implementation to Question 429

--- 429.n-ary-tree-level-order-traversal.java
+++ 429.n-ary-tree-level-order-traversal.java
@@ -26,24 +26,49 @@ class Node {
 
 // Time Complexity: O(n) - n is the number of nodes
 // Space Complexity: O(n)
+// class Solution {
+//     public List<List<Integer>> levelOrder(Node root) {
+//         List<List<Integer>> result = new ArrayList<>();
+//         if (root == null) return result;
+//         Queue<Node> queue = new LinkedList<>();
+//         queue.add(root);
+//         while (!queue.isEmpty()) {
+//             List<Integer> level = new LinkedList();
+//             int size = queue.size();
+//             for (int i = 0; i < size; i++) {
+//                 Node node = queue.poll();
+//                 level.add(node.val);
+//                 queue.addAll(node.children);
+//             }
+//             result.add(level);
+//         }
+//         return result;
+//     }
+// }
+
+// Simplified BFS
+// Time Complexity: O(n), where n is the number of nodes
+// Space Complexity: O(n) same as above, we always have lists containing levels of nodes
 class Solution {
     public List<List<Integer>> levelOrder(Node root) {
         List<List<Integer>> result = new ArrayList<>();
         if (root == null) return result;
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            List<Integer> level = new LinkedList();
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                Node node = queue.poll();
-                level.add(node.val);
-                queue.addAll(node.children);
+        
+        List<Node> previousLayer = Arrays.asList(root);
+        while (!previousLayer.isEmpty()) {
+            List<Node> currentLayer = new ArrayList<>();
+            List<Integer> previousVals = new ArrayList<>();
+            for (Node node: previousLayer) {
+                previousVals.add(node.val);
+                currentLayer.addAll(node.children);
             }
-            result.add(level);
+            result.add(previousVals);
+            previousLayer = currentLayer;
         }
         return result;
     }
 }
+
+
 // @lc code=end
 
