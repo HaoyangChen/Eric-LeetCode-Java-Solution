ef4105e757f37ff8e95a9afab0e0792b56b7372a
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-07 23:34:38 -0700

    Complete Question 116

--- 116.populating-next-right-pointers-in-each-node.java
+++ 116.populating-next-right-pointers-in-each-node.java
@@ -28,21 +28,45 @@ class Node {
 };
 */
 
+// class Solution {
+//     public Node connect(Node root) {
+//         if (root == null) return null;
+//         connectTwoNode(root.left, root.right);
+//         return root;
+//     }
+
+//     private void connectTwoNode(Node node1, Node node2) {
+//         if (node1 == null || node2 == null) {
+//             return;
+//         }
+//         node1.next = node2;
+//         connectTwoNode(node1.left, node1.right);
+//         connectTwoNode(node2.left, node2.right);
+//         connectTwoNode(node1.right, node2.left);
+//     }
+// }
+
+// Time Complexity: O(N) since we traverse each node exactly once
+// Space Complexity: O(N) This is a perfect binary tree which means that the last level contains N/2 nodes. The space complexity for breadth first traversal is the space occupied by the queue which is dependent upon the maximum number of nodes in particular level. 
 class Solution {
     public Node connect(Node root) {
-        if (root == null) return null;
-        connectTwoNode(root.left, root.right);
-        return root;
-    }
-
-    private void connectTwoNode(Node node1, Node node2) {
-        if (node1 == null || node2 == null) {
-            return;
+        if (root == null) return root;
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                if (i < size - 1) {
+                    node.next = queue.peek();
+                }
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
         }
-        node1.next = node2;
-        connectTwoNode(node1.left, node1.right);
-        connectTwoNode(node2.left, node2.right);
-        connectTwoNode(node1.right, node2.left);
+        return root;
     }
 }
 // @lc code=end
