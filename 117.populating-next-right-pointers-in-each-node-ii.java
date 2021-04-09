c3ccad450eb154caded3b13035a7f99aacdf99d0
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-08 23:06:35 -0700

    Add Second Implementation to Question 117

--- 117.populating-next-right-pointers-in-each-node-ii.java
+++ 117.populating-next-right-pointers-in-each-node-ii.java
@@ -28,9 +28,68 @@ class Node {
 };
 */
 
-class Solution {
-    public Node connect(Node root) {
+// Method 1: Level Order Traversal
+// Time Complexity: O(N) since we traverse each node exactly once
+// Space Complexity: O(N) This is a perfect binary tree which means that the last level contains N/2 nodes. The space complexity for breadth first traversal is the space occupied by the queue which is dependent upon the maximum number of nodes in particular level. 
+// class Solution {
+//     public Node connect(Node root) {
+//         if (root == null) return root;
+//         Queue<Node> queue = new LinkedList<Node>();
+//         queue.add(root);
         
+//         while (!queue.isEmpty()) {
+//             int size = queue.size();
+            
+//             for (int i = 0; i < size; i++) {
+//                 Node node = queue.poll();
+//                 if (i < size - 1) {
+//                     node.next = queue.peek();
+//                 }
+//                 if (node.left != null) queue.add(node.left);
+//                 if (node.right != null) queue.add(node.right);
+//             }
+//         }
+//         return root;
+//     }
+// }
+
+
+
+// Method 2: Using previously established next pointers
+// Time Complexity: O(N) since we process each node exactly once
+// Space Complexity: O(1) since we don't make use of any additional data structure for traversing nodes on a particular level like the previous approach does
+class Solution {
+    Node prev, leftmost;
+
+    public void processChild(Node childNode) {
+        if (childNode != null) {
+            if (this.prev != null) {
+                this.prev.next = childNode;
+            } else {
+                this.leftmost = childNode;
+            }
+            this.prev = childNode;
+        }
+    }
+
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        this.leftmost = root;
+        Node curr = leftmost;
+        while (this.leftmost != null) {
+            this.prev = null;
+            curr = this.leftmost;
+            this.leftmost = null;
+
+            while (curr != null) {
+                this.processChild(curr.left);
+                this.processChild(curr.right);
+                curr = curr.next;
+            }
+        }
+        return root;
     }
 }
 // @lc code=end
