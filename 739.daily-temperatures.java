40c6668641b0302f2ea059c16b8a8ac3dda2d702
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-16 23:20:14 -0700

    Add Brute Force Implementation to Question 739

---
+++ 739.daily-temperatures.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=739 lang=java
+ *
+ * [739] Daily Temperatures
+ */
+
+// @lc code=start
+class Solution {
+    public int[] dailyTemperatures(int[] T) {
+        int[] ans = new int[T.length];
+        int[] next = new int[101];
+        Arrays.fill(next, Integer.MAX_VALUE);
+        for (int i = T.length - 1; i >= 0; --i) {
+            int warmer_index = Integer.MAX_VALUE;
+            for (int t = T[i] + 1; t <= 100; ++t) {
+                if (next[t] < warmer_index)
+                    warmer_index = next[t];
+            }
+            if (warmer_index < Integer.MAX_VALUE)
+                ans[i] = warmer_index - i;
+            next[T[i]] = i;
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
