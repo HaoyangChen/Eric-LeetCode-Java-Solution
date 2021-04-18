87a87a5567f1376f80c2e9920c2c7005f50165ff
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-18 15:32:25 -0700

    Complete Question 494

---
+++ 494.target-sum.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=494 lang=java
+ *
+ * [494] Target Sum
+ */
+
+// @lc code=start
+public class Solution {
+    int count = 0;
+    public int findTargetSumWays(int[] nums, int S) {
+        int[][] memo = new int[nums.length][2001];
+        for (int[] row: memo)
+            Arrays.fill(row, Integer.MIN_VALUE);
+        return calculate(nums, 0, 0, S, memo);
+    }
+    public int calculate(int[] nums, int i, int sum, int S, int[][] memo) {
+        if (i == nums.length) {
+            if (sum == S)
+                return 1;
+            else
+                return 0;
+        } else {
+            if (memo[i][sum + 1000] != Integer.MIN_VALUE) {
+                return memo[i][sum + 1000];
+            }
+            int add = calculate(nums, i + 1, sum + nums[i], S, memo);
+            int subtract = calculate(nums, i + 1, sum - nums[i], S, memo);
+            memo[i][sum + 1000] = add + subtract;
+            return memo[i][sum + 1000];
+        }
+    }
+}
+// @lc code=end
+
