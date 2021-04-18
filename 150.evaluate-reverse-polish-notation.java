eb7f49172905261bc8d511e373c0f9b3267578ae
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-17 21:51:02 -0700

    Add Stack Implementation to Question 150

--- 150.evaluate-reverse-polish-notation.java
+++ 150.evaluate-reverse-polish-notation.java
@@ -9,43 +9,76 @@
 // Method 1: Reducing the List In-place
 // Time Complexity: O(n^2)
 // Space Complexity: O(1) - The only extra space used is a constant number of single-value variables. Therefore, the overall algorithm requires O(1) space.
+// class Solution {
+//     public int evalRPN(String[] tokens) {
+//         int currentPosition = 0;
+//         int length = tokens.length;
+//         while (length > 1) {
+//             while (!"+-*/".contains(tokens[currentPosition])) {
+//                 currentPosition++;
+//             }
+//             int number1 = Integer.parseInt(tokens[currentPosition - 2]);
+//             int number2 = Integer.parseInt(tokens[currentPosition - 1]);
+//             int newValue = 0;
+//             switch(tokens[currentPosition]) {
+//                 case "+":
+//                     newValue = number1 + number2;
+//                     break;
+//                 case "-":
+//                     newValue = number1 - number2;
+//                     break;
+//                 case "*":
+//                     newValue = number1 * number2;
+//                     break;
+//                 case "/":
+//                     newValue = number1 / number2;
+//                     break;
+//             }
+//             tokens[currentPosition] = Integer.toString(newValue);
+//             delete2AtIndex(tokens, currentPosition - 2, length);
+//             currentPosition--;
+//             length -= 2;
+//         }
+//         return Integer.parseInt(tokens[0]);
+//     }
+
+//     private void delete2AtIndex(String[] tokens, int d, int length) {
+//         for (int i = d; i < length - 2; i++) {
+//             tokens[i] = tokens[i + 2];
+//         }
+//     }
+// }
+
+
+// Method 2: Evaluate with Stack
 class Solution {
     public int evalRPN(String[] tokens) {
-        int currentPosition = 0;
-        int length = tokens.length;
-        while (length > 1) {
-            while (!"+-*/".contains(tokens[currentPosition])) {
-                currentPosition++;
+        Stack<Integer> stack = new Stack<>();
+        for (String token: tokens) {
+            if (!"+-*/".contains(token)) {
+                stack.push(Integer.valueOf(token));
+                continue;
             }
-            int number1 = Integer.parseInt(tokens[currentPosition - 2]);
-            int number2 = Integer.parseInt(tokens[currentPosition - 1]);
-            int newValue = 0;
-            switch(tokens[currentPosition]) {
+            int number2 = stack.pop();
+            int number1 = stack.pop();
+            int result = 0;
+            switch (token) {
                 case "+":
-                    newValue = number1 + number2;
+                    result = number1 + number2;
                     break;
                 case "-":
-                    newValue = number1 - number2;
+                    result = number1 - number2;
                     break;
                 case "*":
-                    newValue = number1 * number2;
+                    result = number1 * number2;
                     break;
                 case "/":
-                    newValue = number1 / number2;
+                    result = number1 / number2;
                     break;
             }
-            tokens[currentPosition] = Integer.toString(newValue);
-            delete2AtIndex(tokens, currentPosition - 2, length);
-            currentPosition--;
-            length -= 2;
+            stack.push(result);
         }
-        return Integer.parseInt(tokens[0]);
-    }
-
-    private void delete2AtIndex(String[] tokens, int d, int length) {
-        for (int i = d; i < length - 2; i++) {
-            tokens[i] = tokens[i + 2];
-        }
+        return stack.pop();
     }
 }
 // @lc code=end
