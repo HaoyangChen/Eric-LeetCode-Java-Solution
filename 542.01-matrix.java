21de1bf1da6b84905eac41f67b93103f7aa03fa6
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-20 13:56:30 -0700

    Complete Question 542

--- 542.01-matrix.java
+++ 542.01-matrix.java
@@ -5,9 +5,40 @@
  */
 
 // @lc code=start
+
+// Time Complexity: O(r*c), since the new cell are added to the queue only if their current distance is greater than the calculated distance, cells are not likely to be added multiple times
+// Space Complexity: O(r * c)
 class Solution {
+    static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     public int[][] updateMatrix(int[][] matrix) {
-        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dist = new int[m][n];
+        boolean[][] seen = new boolean[m][n];
+        Queue<int[]> queue = new LinkedList<int[]>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    queue.offer(new int[]{i, j});
+                    seen[i][j] = true;
+                }
+            }
+        }
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int i = cell[0];
+            int j = cell[1];
+            for (int d = 0; d < 4; d++) {
+                int ni = i + dirs[d][0];
+                int nj = j + dirs[d][1];
+                if (ni >= 0 && ni < m && nj >= 0 && nj < n && !seen[ni][nj]) {
+                    dist[ni][nj] = dist[i][j] + 1;
+                    queue.offer(new int[]{ni, nj});
+                    seen[ni][nj] = true;
+                }
+            }
+        }
+        return dist;
     }
 }
 // @lc code=end
