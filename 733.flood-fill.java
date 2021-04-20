8c85c96085d374db1bd1e5aec444614b39b0712e
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-19 23:27:46 -0700

    Complete Question 733

---
+++ 733.flood-fill.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=733 lang=java
+ *
+ * [733] Flood Fill
+ */
+
+// @lc code=start
+
+// Time Complexity: O(N), where N is the number of pixels in the image. We might process every pixel.
+// Space Complexity: O(N), the size of the implicit call stack when calling dfs
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int color = image[sr][sc];
+        if (color != newColor) dfs(image, sr, sc, color, newColor);
+        return image;
+    }
+
+    public void dfs(int[][] image, int r, int c, int color, int newColor) {
+        if (image[r][c] == color) {
+            image[r][c] = newColor;
+            if (r >= 1) dfs(image, r - 1, c, color, newColor);
+            if (c >= 1) dfs(image, r, c - 1, color, newColor);
+            if (r + 1 < image.length) dfs(image, r + 1, c, color, newColor);
+            if (c + 1 < image[0].length) dfs(image, r, c + 1, color, newColor);
+        }
+    }
+}
+// @lc code=end
+
