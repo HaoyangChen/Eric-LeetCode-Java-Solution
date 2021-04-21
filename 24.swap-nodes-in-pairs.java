c5e2aaeb64df5436cf7615bcd7bdaa5e8fbe6866
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-21 14:34:28 -0700

    Complete Question 24

---
+++ 24.swap-nodes-in-pairs.java
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=24 lang=java
+ *
+ * [24] Swap Nodes in Pairs
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+ // Time Complexity: O(N) where N is the size of the linked list
+ // Space Complexity: O(N) stack space utilized for recursion
+// class Solution {
+//     public ListNode swapPairs(ListNode head) {
+//         if (head == null || head.next == null) {
+//             return head;
+//         }
+//         // nodes to be swapped
+//         ListNode firstNode = head;
+//         ListNode secondNode = head.next;
+//         // swapping
+//         firstNode.next = swapPairs(secondNode.next);
+//         secondNode.next = firstNode;
+//         return secondNode;
+//     }
+// }
+// @lc code=end
+
+
+// Time Complexity: O(N), where N is the size of the linked list
+// Space Complexity: O(1)
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode prevNode = dummy;
+        while (head != null && head.next != null) {
+            ListNode firstNode = head;
+            ListNode secondNode = head.next;
+            
+            // swapping
+            prevNode.next = secondNode;
+            firstNode.next = secondNode.next;
+            secondNode.next = firstNode;
+
+            // reinitialize the head and prevNode for next step
+            prevNode = firstNode;
+            head = firstNode.next;
+        }
+        return dummy.next;
+    }
+}
