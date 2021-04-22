ec877a31e960ac71337f4a53c08e2212df7f6365
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-22 14:04:44 -0700

    Complete Question 700

--- 700.search-in-a-binary-search-tree.java
+++ 700.search-in-a-binary-search-tree.java
@@ -21,13 +21,27 @@
  * }
  */
 
+// Method 1: Recusion
 // Time Complexity: O(H), where H is the tree height. That results in O(logN) in the average case, and O(N) in the worst case
 // Space Complexity: O(H) to keep the recursion stack. O(logN) in the average case, and O(N) in the worst case
+// class Solution {
+//     public TreeNode searchBST(TreeNode root, int val) {
+//         if (root == null || root.val == val) return root;
+
+//         return val < root.val ? searchBST(root.left, val) :searchBST(root.right, val);
+//     }
+// }
+
+
+// Method 2: Iteration
+// Time complexity: O(H), where H is a tree height
+// Space complexity: O(1) since it's a constant space solution.
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if (root == null || root.val == val) return root;
-
-        return val < root.val ? searchBST(root.left, val) :searchBST(root.right, val);
+        while (root != null && val != root.val) {
+            root = val < root.val ? root.left : root.right;
+        }
+        return root;
     }
 }
 // @lc code=end
