2c6b9521d28c6cc3299bc52ff4cfe42c94f54f93
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-22 00:16:32 -0700

    Add Recursive Approach to Question 206

--- 206.reverse-linked-list.java
+++ 206.reverse-linked-list.java
@@ -15,19 +15,31 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode reverseList(ListNode head) {
-        ListNode oriNext = null;
-        ListNode next = null;
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         ListNode oriNext = null;
+//         ListNode next = null;
 
-        while (head != null) {
-            oriNext = head.next;
-            head.next = next;
-            next = head;
-            head = oriNext;
-        }
-        return next;
-    }
+//         while (head != null) {
+//             oriNext = head.next;
+//             head.next = next;
+//             next = head;
+//             head = oriNext;
+//         }
+//         return next;
+//     }
+// }
+
+// Recursive Approach
+// Time Complexity: O(n). Assume that n is the list's length, the time complexity is O(n)
+// Space Complexity: O(n). The extra space comes from the implicit stack space due to recursion. The recursion can go up to n levels deep.
+public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) return head;
+    ListNode p = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return p;
 }
+
 // @lc code=end
 
