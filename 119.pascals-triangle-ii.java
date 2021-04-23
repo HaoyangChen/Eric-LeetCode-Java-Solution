5a1e1293fec5c57c6c17534982bde411892d0fa8
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-22 20:42:00 -0700

    Add Bottom Up Implementation to Question 509

--- 119.pascals-triangle-ii.java
+++ 119.pascals-triangle-ii.java
@@ -5,9 +5,22 @@
  */
 
 // @lc code=start
+
+// Method 1: Brute Force Recursion
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; i++) {
+            ans.add(getNum(rowIndex, i));
+        }
+        return ans;
+    }
+
+    private int getNum(int row, int col) {
+        if (row == 0 || col == 0 || row == col) {
+            return 1;
+        }
+        return getNum(row - 1, col - 1) + getNum(row - 1, col);
     }
 }
 // @lc code=end
--- 509.fibonacci-number.java
+++ 509.fibonacci-number.java
@@ -9,20 +9,40 @@
 // Method: Iterative Top-Down Approach
 // Time Complexity: O(n). Each value from 2 to n will be visited at least once. The time it takes to do so directly proportionate to n where n is the fibonacci number we are loooking to compute.
 // Space Compelxity: O(1). This requires 1 unit of space for the integer N and 3 units of space to store the computed values (curr, prev1, and prev2) for each loop iteration. The amount of space does not change so it is constant space complexity.
+// class Solution {
+//     public int fib(int n) {
+//         if (n <= 1) return n;
+//         if (n == 2) return 1;
+//         int current = 0;
+//         int prev1 = 1;
+//         int prev2 = 1;
+//         for (int i = 3; i <= n; i++) {
+//             current = prev1 + prev2;
+//             prev2 = prev1;
+//             prev1 = current;
+//         }
+//         return current;
+//     }
+// }
+
+// Bottom-Up Approach using Memorization
+// Time Complexity: O(N) Each number, starting at 2 up to and including N, is visited, computed and then stored for O(1)
+// Space Complexity: O(N) The size of the data structure is proportionate to N
 class Solution {
-    public int fib(int n) {
-        if (n <= 1) return n;
-        if (n == 2) return 1;
-        int current = 0;
-        int prev1 = 1;
-        int prev2 = 1;
-        for (int i = 3; i <= n; i++) {
-            current = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = current;
+    public int fib(int N) {
+        if (N <= 1) return N;
+        return memorize(N);
+    }
+
+    public int memorize(int N) {
+        int[] cache = new int[N + 1];
+        cache[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            cache[i] = cache[i - 1] + cache[i - 2];
         }
-        return current;
+        return cache[N];
     }
 }
+
 // @lc code=end
 
