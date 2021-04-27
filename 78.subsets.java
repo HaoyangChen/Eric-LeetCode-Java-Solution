8e80c778f2ea14788e0c5a70180e59baf9ed3ca3
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-26 17:37:34 -0700

    Add Backtracking Implementation to Question 78

--- 78.subsets.java
+++ 78.subsets.java
@@ -7,26 +7,51 @@
 // @lc code=start
 
 // Shangan Solution
+// class Solution {
+//     public List<List<Integer>> subsets(int[] nums) {
+//         List<List<Integer>> result = new ArrayList<>();
+//         List<Integer> path = new ArrayList<>();
+//         result.add(path);
+//         helper(nums, path, result, 0);
+//         return result;
+//     }
+
+//     private void helper(int[] nums, List<Integer> path, List<List<Integer>> result, int start) {
+//         for (int i = start; i < nums.length; i++) {
+//             if (path.contains(nums[i])) {
+//                 continue;
+//             }
+//             path.add(nums[i]);
+//             result.add(new ArrayList<>(path));
+//             helper(nums, path, result, i + 1);
+//             path.remove(path.size() - 1);
+//         }
+//     }
+// }
+
 class Solution {
+    List<List<Integer>> output = new ArrayList();
+    int n, k;
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> path = new ArrayList<>();
-        result.add(path);
-        helper(nums, path, result, 0);
-        return result;
+        n = nums.length;
+        for (k = 0; k < n + 1; k++) {
+            backtrack(0, new ArrayList<Integer>(), nums);
+        }
+        return output;
     }
 
-    private void helper(int[] nums, List<Integer> path, List<List<Integer>> result, int start) {
-        for (int i = start; i < nums.length; i++) {
-            if (path.contains(nums[i])) {
-                continue;
-            }
-            path.add(nums[i]);
-            result.add(new ArrayList<>(path));
-            helper(nums, path, result, i + 1);
-            path.remove(path.size() - 1);
+    private void backtrack(int first, ArrayList<Integer> curr, int[] nums) {
+        if (curr.size() == k) {
+            output.add(new ArrayList(curr));
+            return;
+        }
+        for (int i = first; i < n; i++) {
+            curr.add(nums[i]);
+            backtrack(i + 1, curr, nums);
+            curr.remove(curr.size() - 1);
         }
     }
 }
+
 // @lc code=end
 
