c3cd82b555e093ac18a93e6f641143eadfcb335e
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-28 00:08:03 -0700

    Add Comment to Question 47

--- 47.permutations-ii.java
+++ 47.permutations-ii.java
@@ -21,6 +21,8 @@ class Solution {
             return;
         }
         for (int i = 0; i < nums.length; i++) {
+            // 剪枝条件：i > 0 是为了保证 nums[i - 1] 有意义
+            // 写 !visited.contains[i - 1] 是因为 nums[i - 1] 在深度优先遍历的过程中刚刚被撤销选择
             if (visited.contains(i) || (i > 0 && nums[i] == nums[i - 1]) && !visited.contains(i - 1)) {
                 continue;
             }
