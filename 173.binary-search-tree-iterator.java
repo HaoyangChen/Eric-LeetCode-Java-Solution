4582a96b59305bafba9ba34d539cd61ea9a5580d
Haoyang Chen <haoyangchen0901@gmail.com>
2021-05-02 21:29:45 -0700

    Add Controlled Recursion Implementation to Question 173

--- 173.binary-search-tree-iterator.java
+++ 173.binary-search-tree-iterator.java
@@ -24,30 +24,59 @@
  // Method 1: Flattening the BST
  // Time Complexity: O(N) is the time taken by the constructor for the iterator
  // Space Complexity: O(N) since we create a new array to contain all the nodes of the BST
+// class BSTIterator {
+//     ArrayList<Integer> nodesSorted;
+//     int index;
+//     public BSTIterator(TreeNode root) {
+//         this.nodesSorted = new ArrayList<Integer>();
+//         this.index = -1;
+//         this._inorder(root);
+//     }
+
+//     private void _inorder(TreeNode root) {
+//         if (root == null) {
+//             return;
+//         }
+//         this._inorder(root.left);
+//         this.nodesSorted.add(root.val);
+//         this._inorder(root.right);
+//     }
+    
+//     public int next() {
+//         return this.nodesSorted.get(++this.index);
+//     }
+    
+//     public boolean hasNext() {
+//         return this.index + 1 < this.nodesSorted.size();
+//     }
+// }
+
+// Method 2: Controlled Recursion
+// Time Complexity:  amortized (average) time complexity for this function would be O(1)
 class BSTIterator {
-    ArrayList<Integer> nodesSorted;
-    int index;
+    Stack<TreeNode> stack;
     public BSTIterator(TreeNode root) {
-        this.nodesSorted = new ArrayList<Integer>();
-        this.index = -1;
-        this._inorder(root);
+        this.stack = new Stack<TreeNode>();
+        this._leftMostInoder(root);
     }
 
-    private void _inorder(TreeNode root) {
-        if (root == null) {
-            return;
+    private void _leftMostInoder(TreeNode root) {
+        while (root != null) {
+            this.stack.push(root);
+            root = root.left;
         }
-        this._inorder(root.left);
-        this.nodesSorted.add(root.val);
-        this._inorder(root.right);
     }
-    
+
     public int next() {
-        return this.nodesSorted.get(++this.index);
+        TreeNode topMostNode = this.stack.pop();
+        if (topMostNode != null) {
+            this._leftMostInoder(topMostNode.right);
+        }
+        return topMostNode.val;
     }
-    
+
     public boolean hasNext() {
-        return this.index + 1 < this.nodesSorted.size();
+        return this.stack.size() > 0;
     }
 }
 
