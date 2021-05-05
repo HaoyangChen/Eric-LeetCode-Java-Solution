663e53c803545e1c3edff55a6487bb298bb9913a
Haoyang Chen <haoyangchen0901@gmail.com>
2021-05-05 14:40:57 -0700

    Add Inorder Simulation to Question 109

--- 109.convert-sorted-list-to-binary-search-tree.java
+++ 109.convert-sorted-list-to-binary-search-tree.java
@@ -65,49 +65,101 @@
 // Method 2: Recursion + Conversion to Array
 // Time Complexity: O(N) time complexity is O(N) since we convert the linked list to an array initially and then convert the array into a BST. Accessing the middle element now takes O(1) and thus the time complexity comes down
 // Space Complexity: O(N) Since we used extra space to bring down the time complexity, the space complexity now goes up to O(N) as opposed of O(logN) because of the array we construct initially
-class Solution {
-    private List<Integer> values;
+// class Solution {
+//     private List<Integer> values;
 
-    public Solution() {
-      this.values = new ArrayList<Integer>();
-    }
+//     public Solution() {
+//       this.values = new ArrayList<Integer>();
+//     }
   
-    private void mapListToValues(ListNode head) {
-      while (head != null) {
-        this.values.add(head.val);
-        head = head.next;
+//     private void mapListToValues(ListNode head) {
+//       while (head != null) {
+//         this.values.add(head.val);
+//         head = head.next;
+//       }
+//     }
+  
+//     private TreeNode convertListToBST(int left, int right) {
+//       // Invalid case
+//       if (left > right) {
+//         return null;
+//       }
+  
+//       // Middle element forms the root.
+//       int mid = (left + right) / 2;
+//       TreeNode node = new TreeNode(this.values.get(mid));
+  
+//       // Base case for when there is only one element left in the array
+//       if (left == right) {
+//         return node;
+//       }
+  
+//       // Recursively form BST on the two halves
+//       node.left = convertListToBST(left, mid - 1);
+//       node.right = convertListToBST(mid + 1, right);
+//       return node;
+//     }
+  
+//     public TreeNode sortedListToBST(ListNode head) {
+  
+//       // Form an array out of the given linked list and then
+//       // use the array to form the BST.
+//       this.mapListToValues(head);
+  
+//       // Convert the array to
+//       return convertListToBST(0, this.values.size() - 1);
+//     }
+//   }
+
+
+// Inorder Simulation
+// Time Complexity: The time complexity is still O(N) since we still have to process each of the nodes in the linked list once and form corresponding BST nodes.
+// Space Complexity: O(logN) since now the only extra space is used by the recursion stack and since we are building a height balanced BST, the height is bounded by logN.
+class Solution {
+    private ListNode head;
+  
+    private int findSize(ListNode head) {
+      ListNode ptr = head;
+      int c = 0;
+      while (ptr != null) {
+        ptr = ptr.next;  
+        c += 1;
       }
+      return c;
     }
   
-    private TreeNode convertListToBST(int left, int right) {
+    private TreeNode convertListToBST(int l, int r) {
       // Invalid case
-      if (left > right) {
+      if (l > r) {
         return null;
       }
   
-      // Middle element forms the root.
-      int mid = (left + right) / 2;
-      TreeNode node = new TreeNode(this.values.get(mid));
+      int mid = (l + r) / 2;
   
-      // Base case for when there is only one element left in the array
-      if (left == right) {
-        return node;
-      }
+      // First step of simulated inorder traversal. Recursively form
+      // the left half
+      TreeNode left = this.convertListToBST(l, mid - 1);
   
-      // Recursively form BST on the two halves
-      node.left = convertListToBST(left, mid - 1);
-      node.right = convertListToBST(mid + 1, right);
+      // Once left half is traversed, process the current node
+      TreeNode node = new TreeNode(this.head.val);
+      node.left = left;
+  
+      // Maintain the invariance mentioned in the algorithm
+      this.head = this.head.next;
+  
+      // Recurse on the right hand side and form BST out of them
+      node.right = this.convertListToBST(mid + 1, r);
       return node;
     }
   
     public TreeNode sortedListToBST(ListNode head) {
+      // Get the size of the linked list first
+      int size = this.findSize(head);
   
-      // Form an array out of the given linked list and then
-      // use the array to form the BST.
-      this.mapListToValues(head);
+      this.head = head;
   
-      // Convert the array to
-      return convertListToBST(0, this.values.size() - 1);
+      // Form the BST now that we know the size
+      return convertListToBST(0, size - 1);
     }
   }
 // @lc code=end
