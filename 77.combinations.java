e851b130e560fb6c210858f8f90988380dcc5715
Haoyang Chen <haoyangchen0901@gmail.com>
2021-05-06 18:53:42 -0700

    Complete Question 77 with Backtracking Method

--- 77.combinations.java
+++ 77.combinations.java
@@ -6,8 +6,26 @@
 
 // @lc code=start
 class Solution {
+    List<List<Integer>> result = new LinkedList<>();
+    int n;
+    int k;
+
     public List<List<Integer>> combine(int n, int k) {
-        
+        this.n = n;
+        this.k = k;
+        backtrack(1, new LinkedList<Integer>());
+        return result;
+    }
+
+    private void backtrack(int first, LinkedList<Integer> curr) {
+        if (curr.size() == k) {
+            result.add(new LinkedList<>(curr));
+        }
+        for (int i = first; i < n + 1; i++) {
+            curr.add(i);
+            backtrack(i + 1, curr);
+            curr.removeLast();
+        }
     }
 }
 // @lc code=end
