504a79cfda4a2c7f16016d031b1abd369e97ee2c
Haoyang Chen <haoyangchen0901@gmail.com>
2021-05-07 22:41:50 -0700

    Add Iteration Implementation to Question 100

--- 100.same-tree.java
+++ 100.same-tree.java
@@ -24,15 +24,60 @@
 // Time Complexity: O(N), where N is a number of nodes in the tree, since one visits each node                       exactly once.
 // Space Complexity: O(log(N)) in the best case of completely balanced tree and O(N) in the worst 
 //                   case of completely unbalanced tree, to keep a recursion stack
+// class Solution {
+//     public boolean isSameTree(TreeNode p, TreeNode q) {
+//         if (p == null || q == null) {
+//             return p == q;
+//         }
+//         if (p.val != q.val) {
+//             return false;
+//         }
+//         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+//     }
+// }
+
+
+
+// Method 2: Iteration
+// Time Complexity: O(N) since each node is visited exactly once
+// Space Complexity: O(log(N)) in the best case of completely balanced tree and O(N) in the worst case of completely unbalanced tree, to keep a deque
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null || q == null) {
-            return p == q;
+        if (p == null && q == null) return true;
+        if (!check(p, q)) return false;
+
+        ArrayDeque<TreeNode> deqP = new ArrayDeque<TreeNode>();
+        ArrayDeque<TreeNode> deqQ = new ArrayDeque<TreeNode>();
+        deqP.addLast(p);
+        deqQ.addLast(q);
+
+        while (!deqP.isEmpty()) {
+            p = deqP.removeFirst();
+            q = deqQ.removeFirst();
+            if (!check(p, q)) return false;
+            if (p != null) {
+                if (!check(p.left, q.left)) return false;
+                if (p.left != null) {
+                    deqP.addLast(p.left);
+                    deqQ.addLast(q.left);
+                }
+    
+                if (!check(p.right, q.right)) return false;
+                if (p.right != null) {
+                    deqP.addLast(p.right);
+                    deqQ.addLast(q.right);
+                }
+            }
         }
-        if (p.val != q.val) {
-            return false;
-        }
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return true;
+    }
+
+
+    public boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return true;
     }
 }
 // @lc code=end
