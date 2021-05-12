5c0736e7e194f2a62d9bf470481918030816183f
Haoyang Chen <haoyangchen0901@gmail.com>
2021-05-11 18:00:43 -0700

    Complete Question 219

--- 219.contains-duplicate-ii.java
+++ 219.contains-duplicate-ii.java
@@ -5,9 +5,20 @@
  */
 
 // @lc code=start
+
+// Time Complexity: O(nlog(min(k, n))) . We do n operations of search, delete and insert. Each operation costs logarithmic time complexity in the sliding window which size is min(k,n).
+// Space Complexity: O(min(k, n)) - Space is the size of the sliding window which should not exceed n or k
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        
+        Set<Integer> set = new TreeSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) return true;
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
     }
 }
 // @lc code=end
