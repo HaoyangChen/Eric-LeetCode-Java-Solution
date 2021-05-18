fe677ecb84caf3b696aa52d97620103ceae1ce26
Haoyang Chen <haoyangchen0901@gmail.com>
2021-05-18 12:19:02 -0700

    Add 2 pointers Implementation to Question 42

--- 42.trapping-rain-water.java
+++ 42.trapping-rain-water.java
@@ -9,21 +9,43 @@
 // Using Monotonous Stack
 // Time Complexity: O(n) single iteration of O(n) in which bar can be touched at most twice(due to insertion and deletion from stack) and insertion and deletion from stack takes O(1)
 // Space Complexity: O(n) stack can take up to O(n) space in case of stairs-like or flat structure
+// class Solution {
+//     public int trap(int[] height) {
+//         int ans = 0;
+//         Deque<Integer> stack = new LinkedList<Integer>();
+//         int n = height.length;
+//         for (int i = 0; i < n; i++) {
+//             while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+//                 int top = stack.pop();
+//                 if (stack.isEmpty()) break;
+//                 int left = stack.peek();
+//                 int currentWidth = i - left - 1;
+//                 int currentHeight = Math.min(height[left], height[i]) - height[top];
+//                 ans += currentWidth * currentHeight;
+//             }
+//             stack.push(i);
+//         }
+//         return ans;
+//     }
+// }
+
+
+// Method2: Using 2 Pointers
 class Solution {
     public int trap(int[] height) {
         int ans = 0;
-        Deque<Integer> stack = new LinkedList<Integer>();
-        int n = height.length;
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
-                int top = stack.pop();
-                if (stack.isEmpty()) break;
-                int left = stack.peek();
-                int currentWidth = i - left - 1;
-                int currentHeight = Math.min(height[left], height[i]) - height[top];
-                ans += currentWidth * currentHeight;
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        while (left < right) {
+            leftMax = Math.max(height[left], leftMax);
+            rightMax = Math.max(height[right], rightMax);
+            if (height[left] < height[right]) {
+                ans += leftMax - height[left];
+                left++;
+            } else {
+                ans += rightMax - height[right];
+                right--;
             }
-            stack.push(i);
         }
         return ans;
     }
