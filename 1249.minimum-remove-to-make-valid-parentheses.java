71e67692af10bb1b3235e30fe4d2b83ff878330f
Haoyang Chen <haoyangchen0901@gmail.com>
2021-05-20 23:44:21 -0700

    Add Stack and String Builder Implementation to Question 1249

--- 1249.minimum-remove-to-make-valid-parentheses.java
+++ 1249.minimum-remove-to-make-valid-parentheses.java
@@ -7,9 +7,35 @@
 // @lc code=start
 
 // Method 1: Using a Stack and String Builder
+// Time Complexity: O(n), where n is the length of the input string
+// Space Complexity: O(n), we are using a stack, set, and string builder. Each of which could have up to n characters in them, and so requires up to O(n)
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        
+        Set<Integer> indexesToRemove = new HashSet<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            }
+            if (s.charAt(i) == ')') {
+                if (stack.isEmpty()) {
+                    indexesToRemove.add(i);
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            indexesToRemove.add(stack.pop());
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (!indexesToRemove.contains(i)) {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 }
 // @lc code=end
