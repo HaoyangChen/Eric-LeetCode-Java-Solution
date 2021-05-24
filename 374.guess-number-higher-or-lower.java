b9228606934a82726435fc8c401507997ae33f35
Haoyang Chen <haoyangchen0901@gmail.com>
2021-05-23 22:39:01 -0700

    Complete Question 374 with Ternary Search Implmentation

--- 374.guess-number-higher-or-lower.java
+++ 374.guess-number-higher-or-lower.java
@@ -18,19 +18,49 @@
  // Method: Binary Search
  // Time Complexity: O(logN) Binary search is used
  // Space Complexity: O(1) No extra space is used
+// public class Solution extends GuessGame {
+//     public int guessNumber(int n) {
+//         int low = 1;
+//         int high = n;
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             int res = guess(mid);
+//             if (res < 0) {
+//                 high = mid - 1;
+//             } else if (res == 0) {
+//                 return mid;
+//             } else {
+//                 low = mid + 1;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+// Method 2: Ternary Search
+// Time complexity: O(log3n). Ternary Search is used
+// Space complexity: O(1). No extra space is used.
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
         int low = 1;
         int high = n;
         while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int res = guess(mid);
-            if (res < 0) {
-                high = mid - 1;
-            } else if (res == 0) {
-                return mid;
-            } else {
-                low = mid + 1;
+            int mid1 = low + (high - low) / 3;
+            int mid2 = high - (high - low) / 3;
+            int res1 = guess(mid1);
+            int res2 = guess(mid2);
+            if (res1 == 0)
+                return mid1;
+            if (res2 == 0)
+                return mid2;
+            else if (res1 < 0)
+                high = mid1 - 1;
+            else if (res2 > 0)
+                low = mid2 + 1;
+            else {
+                low = mid1 + 1;
+                high = mid2 - 1;
             }
         }
         return -1;
