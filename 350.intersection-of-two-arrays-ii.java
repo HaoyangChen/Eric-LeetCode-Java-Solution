5cc672caa4e121d1c4f299244e650371a807c281
Haoyang Chen <haoyangchen0901@gmail.com>
2021-05-28 21:43:15 -0700

    Complete Question 350 with Hash Map

---
+++ 350.intersection-of-two-arrays-ii.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=350 lang=java
+ *
+ * [350] Intersection of Two Arrays II
+ */
+
+// @lc code=start
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int n: nums1) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        int k = 0;
+        for (int n: nums2) {
+            int cnt = map.getOrDefault(n, 0);
+            if (cnt > 0) {
+                nums1[k++] = n;
+                map.put(n, cnt - 1);
+            }
+        }
+        return Arrays.copyOfRange(nums1, 0, k);
+    }
+}
+// @lc code=end
+
