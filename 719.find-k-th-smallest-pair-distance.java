6d4533f8c1d52422f1803049a0f6e00d77bdef91
Haoyang Chen <haoyangchen0901@gmail.com>
2021-05-29 23:57:44 -0700

    Complete Question 719

---
+++ 719.find-k-th-smallest-pair-distance.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=719 lang=java
+ *
+ * [719] Find K-th Smallest Pair Distance
+ */
+
+// @lc code=start
+class Solution {
+    public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+        PriorityQueue<Node> heap = new PriorityQueue<Node>(nums.length,
+            Comparator.<Node> comparingInt(node -> nums[node.nei] - nums[node.root]));
+        for (int i = 0; i + 1 < nums.length; ++i) {
+            heap.offer(new Node(i, i+1));
+        }
+
+        Node node = null;
+        for (; k > 0; --k) {
+            node = heap.poll();
+            if (node.nei + 1 < nums.length) {
+                heap.offer(new Node(node.root, node.nei + 1));
+            }
+        }
+        return nums[node.nei] - nums[node.root];
+    }
+}
+class Node {
+    int root;
+    int nei;
+    Node(int r, int n) {
+        root = r;
+        nei = n;
+    }
+}
+// @lc code=end
+
