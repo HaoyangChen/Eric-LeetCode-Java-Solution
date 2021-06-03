64ab135bfa476f01b9c12baeca4259faf06d7235
Haoyang Chen <haoyangchen0901@gmail.com>
2021-06-03 00:03:11 -0700

    Complete Question 437 with Prefix Sum

--- 437.path-sum-iii.java
+++ 437.path-sum-iii.java
@@ -20,9 +20,30 @@
  *     }
  * }
  */
+
+// Method: Prefix Sum
+// Time Complexity: where N is a number of nodes. During preorder traversal, each node is visited once.
+// Space Complexity: Up to O(N) to keep the hashmap of prefix sums, where N is a number of nodes
 class Solution {
+    int count = 0;
+    int k;
+    HashMap<Integer, Integer> h = new HashMap();
+
     public int pathSum(TreeNode root, int targetSum) {
-        
+        k = targetSum;
+        preorder(root, 0);
+        return count;
+    }
+
+    private void preorder(TreeNode node, int currSum) {
+        if (node == null) return;
+        currSum += node.val;
+        if (currSum == k) count++;
+        count += h.getOrDefault(currSum - k, 0);
+        h.put(currSum, h.getOrDefault(currSum, 0) + 1);
+        preorder(node.left, currSum);
+        preorder(node.right, currSum);
+        h.put(currSum, h.get(currSum) - 1);
     }
 }
 // @lc code=end
