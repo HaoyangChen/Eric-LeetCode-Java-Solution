12e192a463af03e81bf718facf2e489289b5891e
Haoyang Chen <haoyangchen0901@gmail.com>
2021-06-08 23:49:44 -0700

    Complete Question 1046

--- 1046.last-stone-weight.java
+++ 1046.last-stone-weight.java
@@ -6,23 +6,69 @@
 
 // @lc code=start
 
-// Heap-Based Simulation
+// Approach 1: Heap-Based Simulation
 // Time Complexity: O(NlogN)
 // Space complexity : O(N) or O(logN).
+
+// class Solution {
+//     public int lastStoneWeight(int[] stones) {
+//         PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
+//         for (int stone: stones) {
+//             heap.add(stone);
+//         }
+//         while (heap.size() > 1) {
+//             int stone1 = heap.poll();
+//             int stone2 = heap.poll();
+//             if (stone1 != stone2) {
+//                 heap.add(stone1 - stone2);
+//             }
+//         }
+//         return heap.isEmpty() ? 0 : heap.remove();
+//     }
+// }
+
+
+// Approach 2: Bucket Sort
+// Time complexity: O(N+W)
+// Space complexity: O(W).
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
+        
+        // Set up the bucket array.
+        int maxWeight = stones[0];
         for (int stone: stones) {
-            heap.add(stone);
+            maxWeight = Math.max(maxWeight, stone);
         }
-        while (heap.size() > 1) {
-            int stone1 = heap.poll();
-            int stone2 = heap.poll();
-            if (stone1 != stone2) {
-                heap.add(stone1 - stone2);
+        int[] buckets = new int[maxWeight + 1];
+
+        // Bucket sort.
+        for (int weight : stones) {
+            buckets[weight]++;
+        }
+
+        // Scan through the buckets.
+        int biggestWeight = 0;
+        int currentWeight = maxWeight;
+        while (currentWeight > 0) {
+            if (buckets[currentWeight] == 0) {
+                currentWeight--;
+            } else if (biggestWeight == 0) {
+                buckets[currentWeight] %= 2;
+                if (buckets[currentWeight] == 1) {
+                    biggestWeight = currentWeight;
+                }
+                currentWeight--;
+            } else {
+                buckets[currentWeight]--;
+                if (biggestWeight - currentWeight <= currentWeight) {
+                    buckets[biggestWeight - currentWeight]++;
+                    biggestWeight = 0;
+                } else {
+                    biggestWeight -= currentWeight;
+                }
             }
         }
-        return heap.isEmpty() ? 0 : heap.remove();
+        return biggestWeight;
     }
 }
 // @lc code=end
