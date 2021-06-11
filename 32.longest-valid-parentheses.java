80bca2346b207c22afe638d60b7e5fc28aca1bf7
Haoyang Chen <haoyangchen0901@gmail.com>
2021-06-10 23:59:11 -0700

    Complete Question 32

---
+++ 32.longest-valid-parentheses.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=32 lang=java
+ *
+ * [32] Longest Valid Parentheses
+ */
+
+// @lc code=start
+public class Solution {
+
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
+    }
+}
+// @lc code=end
+
