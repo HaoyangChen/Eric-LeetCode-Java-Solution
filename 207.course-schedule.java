4481d799bf04b970ccfaf4030e3c50f532fd33cc
Haoyang Chen <haoyangchen0901@gmail.com>
2021-06-12 11:00:47 -0700

    Add DFS PostOrder Implementation to Question 207

--- 207.course-schedule.java
+++ 207.course-schedule.java
@@ -118,5 +118,57 @@
 //         return result;
 //     }
 // }
+
+
+
+// Postorder DFS Method
+// Time Complexity: O(|E| + |V|) where |V| is the numebr of vertices and |E| is the number of dependencies
+// Space Complexity: O(|E| + |V|)
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        HashMap<Integer, List<Integer>> courseDict = new HashMap<>();
+        for (int[] relation: prerequisites) {
+            if (courseDict.containsKey(relation[1])) {
+                courseDict.get(relation[1]).add(relation[0]);
+            } else {
+                List<Integer> nextCourses = new LinkedList<>();
+                nextCourses.add(relation[0]);
+                courseDict.put(relation[1], nextCourses);
+            }
+        }
+
+        boolean[] checked = new boolean[numCourses];
+        boolean[] path = new boolean[numCourses];
+
+        for (int currCourse = 0; currCourse < numCourses; currCourse++) {
+            if (this.isCyclic(currCourse, courseDict, checked, path)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected boolean isCyclic(int currCourse, HashMap<Integer, List<Integer>> courseDict, boolean[] checked, boolean[] path) {
+        if (checked[currCourse]) {
+            return false;
+        }
+        if (path[currCourse]) {
+            return true;
+        }
+        if (!courseDict.containsKey(currCourse)) {
+            return false;
+        }
+        path[currCourse] = true;
+        boolean result = false;
+        for (Integer child: courseDict.get(currCourse)) {
+            result = this.isCyclic(child, courseDict, checked, path);
+            if (result) break;
+        }
+        path[currCourse] = false;
+        checked[currCourse] = true;
+        return result;
+    }
+}
+
 // @lc code=end
 
