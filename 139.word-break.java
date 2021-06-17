0ce58ae4f157373d23fb27efe623633b0408b368
Haoyang Chen <haoyangchen0901@gmail.com>
2021-06-17 00:26:35 -0700

    Complete Question 139 with BFS Implementation

--- 139.word-break.java
+++ 139.word-break.java
@@ -23,27 +23,54 @@
 //     }
 // }
 
-// Recursion with memoization
+// Method: Recursion with memoization
 // Time Complexity: O(n^3) - size of the recursion tree can go up to n^2
 // Space Complexity: O(n) - the depth of recursion tree can go up to n
-public class Solution {
-    public boolean wordBreak(String s, List<String> wordDict) {
-        return wordBreakMemo(s, new HashSet<>(wordDict), 0, new Boolean[s.length()]);
-    }
+// public class Solution {
+//     public boolean wordBreak(String s, List<String> wordDict) {
+//         return wordBreakMemo(s, new HashSet<>(wordDict), 0, new Boolean[s.length()]);
+//     }
 
-    private boolean wordBreakMemo(String s, Set<String> wordDict, int start, Boolean[] memo) {
-        if (start == s.length()) {
-            return true;
-        }
-        if (memo[start] != null) {
-            return memo[start];
-        }
-        for (int end = start + 1; end <= s.length(); end++) {
-            if (wordDict.contains(s.substring(start, end)) && wordBreakMemo(s, wordDict, end, memo)) {
-                return memo[start] = true;
+//     private boolean wordBreakMemo(String s, Set<String> wordDict, int start, Boolean[] memo) {
+//         if (start == s.length()) {
+//             return true;
+//         }
+//         if (memo[start] != null) {
+//             return memo[start];
+//         }
+//         for (int end = start + 1; end <= s.length(); end++) {
+//             if (wordDict.contains(s.substring(start, end)) && wordBreakMemo(s, wordDict, end, memo)) {
+//                 return memo[start] = true;
+//             }
+//         }
+//         return memo[start] = false;
+//     }
+// }
+
+
+// Method 2: Using Breadth-First-Search
+// Time Complexity: O(n^3) - For every starting index, the search can continue till the end of the given string.
+// Space Complexity: O(n) - Queue of at most n size is needed.
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet = new HashSet<>(wordDict);
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[s.length()];
+        queue.add(0);
+        while (!queue.isEmpty()) {
+            int start = queue.remove();
+            if (visited[start]) continue;
+            for (int end = start + 1; end <= s.length(); end++) {
+                if (wordDictSet.contains(s.substring(start, end))) {
+                    queue.add(end);
+                    if (end == s.length()) {
+                        return true;
+                    }
+                }
             }
+            visited[start] = true;
         }
-        return memo[start] = false;
+        return false;
     }
 }
 
