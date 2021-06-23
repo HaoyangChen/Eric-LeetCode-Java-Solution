a392fbe5c988ef2c915a1c1b2754ea011a45e6b8
Haoyang Chen <haoyangchen0901@gmail.com>
2021-06-22 20:56:09 -0700

    Add Doubly Linked List to Question 707

--- 707.design-linked-list.java
+++ 707.design-linked-list.java
@@ -9,69 +9,192 @@
 // Method 1: Singly Linked List
 // Time Complexity: O(1) for addAtHead. O(k) for get, addAtIndex, and Delete AtIndex, where k is the index of the element to get, add, or delete. O(N) for addAtTail.
 // Space Complexity: O(1) for all operations
+// public class ListNode {
+//     int val;
+//     ListNode next;
+//     ListNode(int x) {
+//         val = x;
+//     }
+// }
+
+// class MyLinkedList {
+//     int size;
+//     ListNode head;
+
+//     /** Initialize your data structure here. */
+//     public MyLinkedList() {
+//         size = 0;
+//         head = new ListNode(0);
+//     }
+    
+//     /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+//     public int get(int index) {
+//         if (index < 0 || index >= size) return -1;
+//         ListNode curr = head;
+//         for (int i = 0; i < index + 1; i++) {
+//             curr = curr.next;
+//         }
+//         return curr.val;
+//     }
+    
+//     /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+//     public void addAtHead(int val) {
+//         addAtIndex(0, val);
+//     }
+    
+//     /** Append a node of value val to the last element of the linked list. */
+//     public void addAtTail(int val) {
+//         addAtIndex(size, val);
+//     }
+    
+//     /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+//     public void addAtIndex(int index, int val) {
+//         if (index > size) return;
+//         if (index < 0) index = 0;
+//         ++size;
+//         ListNode pred = head;
+//         for (int i = 0; i < index; i++) {
+//             pred = pred.next;
+//         }
+//         ListNode toAdd = new ListNode(val);
+//         toAdd.next = pred.next;
+//         pred.next = toAdd;
+//     }
+    
+//     /** Delete the index-th node in the linked list, if the index is valid. */
+//     public void deleteAtIndex(int index) {
+//         if (index < 0 || index >= size) return;
+//         size--;
+//         ListNode pred = head;
+//         for (int i = 0; i < index; i++) {
+//             pred = pred.next;
+//         }
+//         pred.next = pred.next.next;
+//     }
+// }
+
+
+// Method 2: Doubly Linked List
+// Time Complexity: O(1) for addHead and addTail. O(min(k, N - k)) for get, addAtIdex, and deleteAtIndex, where k is an index of the element to get, add, or delete.
+// Space Complexity: O(1) for all operations
 public class ListNode {
     int val;
     ListNode next;
-    ListNode(int x) {
-        val = x;
-    }
-}
-
-class MyLinkedList {
+    ListNode prev;
+    ListNode(int x) { val = x; }
+  }
+  
+  class MyLinkedList {
     int size;
-    ListNode head;
-
-    /** Initialize your data structure here. */
+    // sentinel nodes as pseudo-head and pseudo-tail
+    ListNode head, tail;
     public MyLinkedList() {
-        size = 0;
-        head = new ListNode(0);
+      size = 0;
+      head = new ListNode(0);
+      tail = new ListNode(0);
+      head.next = tail;
+      tail.prev = head;
     }
-    
+  
     /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
     public int get(int index) {
-        if (index < 0 || index >= size) return -1;
-        ListNode curr = head;
-        for (int i = 0; i < index + 1; i++) {
-            curr = curr.next;
-        }
-        return curr.val;
+      // if index is invalid
+      if (index < 0 || index >= size) return -1;
+  
+      // choose the fastest way: to move from the head
+      // or to move from the tail
+      ListNode curr = head;
+      if (index + 1 < size - index)
+        for(int i = 0; i < index + 1; ++i) curr = curr.next;
+      else {
+        curr = tail;
+        for(int i = 0; i < size - index; ++i) curr = curr.prev;
+      }
+  
+      return curr.val;
     }
-    
+  
     /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
     public void addAtHead(int val) {
-        addAtIndex(0, val);
+      ListNode pred = head, succ = head.next;
+  
+      ++size;
+      ListNode toAdd = new ListNode(val);
+      toAdd.prev = pred;
+      toAdd.next = succ;
+      pred.next = toAdd;
+      succ.prev = toAdd;
     }
-    
+  
     /** Append a node of value val to the last element of the linked list. */
     public void addAtTail(int val) {
-        addAtIndex(size, val);
+      ListNode succ = tail, pred = tail.prev;
+  
+      ++size;
+      ListNode toAdd = new ListNode(val);
+      toAdd.prev = pred;
+      toAdd.next = succ;
+      pred.next = toAdd;
+      succ.prev = toAdd;
     }
-    
+  
     /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
     public void addAtIndex(int index, int val) {
-        if (index > size) return;
-        if (index < 0) index = 0;
-        ++size;
-        ListNode pred = head;
-        for (int i = 0; i < index; i++) {
-            pred = pred.next;
-        }
-        ListNode toAdd = new ListNode(val);
-        toAdd.next = pred.next;
-        pred.next = toAdd;
+      // If index is greater than the length, 
+      // the node will not be inserted.
+      if (index > size) return;
+  
+      // [so weird] If index is negative, 
+      // the node will be inserted at the head of the list.
+      if (index < 0) index = 0;
+  
+      // find predecessor and successor of the node to be added
+      ListNode pred, succ;
+      if (index < size - index) {
+        pred = head;
+        for(int i = 0; i < index; ++i) pred = pred.next;
+        succ = pred.next;
+      }
+      else {
+        succ = tail;
+        for (int i = 0; i < size - index; ++i) succ = succ.prev;
+        pred = succ.prev;
+      }
+  
+      // insertion itself
+      ++size;
+      ListNode toAdd = new ListNode(val);
+      toAdd.prev = pred;
+      toAdd.next = succ;
+      pred.next = toAdd;
+      succ.prev = toAdd;
     }
-    
+  
     /** Delete the index-th node in the linked list, if the index is valid. */
     public void deleteAtIndex(int index) {
-        if (index < 0 || index >= size) return;
-        size--;
-        ListNode pred = head;
-        for (int i = 0; i < index; i++) {
-            pred = pred.next;
-        }
-        pred.next = pred.next.next;
+      // if the index is invalid, do nothing
+      if (index < 0 || index >= size) return;
+  
+      // find predecessor and successor of the node to be deleted
+      ListNode pred, succ;
+      if (index < size - index) {
+        pred = head;
+        for(int i = 0; i < index; ++i) pred = pred.next;
+        succ = pred.next.next;
+      }
+      else {
+        succ = tail;
+        for (int i = 0; i < size - index - 1; ++i) succ = succ.prev;
+        pred = succ.prev.prev;
+      }
+  
+      // delete pred.next 
+      --size;
+      pred.next = succ;
+      succ.prev = pred;
     }
-}
+  }
+
 
 /**
  * Your MyLinkedList object will be instantiated and called as such:
