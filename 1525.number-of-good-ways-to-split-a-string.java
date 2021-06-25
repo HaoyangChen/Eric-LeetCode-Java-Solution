205b6ab5cfff264523ef3dbeb2430c834e5d7c68
Haoyang Chen <haoyangchen0901@gmail.com>
2021-06-24 22:46:08 -0700

    Complete Question 203 with Sentinel Node

---
+++ 1525.number-of-good-ways-to-split-a-string.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1525 lang=java
+ *
+ * [1525] Number of Good Ways to Split a String
+ */
+
+// @lc code=start
+class Solution {
+    public int numSplits(String s) {
+        
+    }
+}
+// @lc code=end
+
--- 203.remove-linked-list-elements.java
+++ 203.remove-linked-list-elements.java
@@ -15,9 +15,22 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ // Method Sentinel Node
+ // Time Complexity: O(N) - it's one pass solution
+ // Space Complexity: O(1) - it's a constant space solution
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
+        ListNode sentinel = new ListNode(0);
+        sentinel.next = head;
         
+        ListNode prev = sentinel, curr = head;
+        while (curr != null) {
+            if (curr.val == val) prev.next = curr.next;
+            else prev = curr;
+            curr = curr.next;
+        }
+        return sentinel.next;
     }
 }
 // @lc code=end
