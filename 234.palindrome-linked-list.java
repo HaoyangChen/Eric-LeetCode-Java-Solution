cefe66447cf1e0cbc2e330f6c0f8a044b5105da5
Haoyang Chen <haoyangchen0901@gmail.com>
2021-06-25 21:15:34 -0700

    Add Two Pointer Technique to Question 234

--- 234.palindrome-linked-list.java
+++ 234.palindrome-linked-list.java
@@ -17,41 +17,66 @@
  */
 
  // Shangan Solution
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+//         ListNode slow = head;
+//         ListNode fast = head;
+//         while (fast != null && fast.next != null) {
+//             fast = fast.next.next;
+//             slow = slow.next;
+//         }
+//         // This part is a bit tricky -- for list with even number of elements  !important
+//         if (fast != null) {
+//             slow = slow.next;
+//         }
+//         slow = reverse(slow);
+//         ListNode iterator = head;
+//         while (slow != null) {
+//             if (iterator.val != slow.val) {
+//                 return false;
+//             }
+//             iterator = iterator.next;
+//             slow = slow.next;
+//         }
+//         return true;
+//     }
+
+//     private ListNode reverse(ListNode head) {
+//         ListNode oriNext = null;
+//         ListNode next = null;
+
+//         while (head != null) {
+//             oriNext = head.next;
+//             head.next = next;
+//             next = head;
+//             head = oriNext;
+//         }
+//         return next;
+//     }
+// }
+
+
+// Copy into Array List and then Use Two Pointer Technique
+// Time Complexity: O(n), where n is the number of nodes in the linked list
+// Space Complexity: O(n)
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
+        List<Integer> vals = new ArrayList<>();
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            vals.add(currentNode.val);
+            currentNode = currentNode.next;
         }
-        // This part is a bit tricky -- for list with even number of elements  !important
-        if (fast != null) {
-            slow = slow.next;
-        }
-        slow = reverse(slow);
-        ListNode iterator = head;
-        while (slow != null) {
-            if (iterator.val != slow.val) {
+        int front = 0;
+        int back = vals.size() - 1;
+        while (front < back) {
+            if (!vals.get(front).equals(vals.get(back))) {
                 return false;
             }
-            iterator = iterator.next;
-            slow = slow.next;
+            front++;
+            back--;
         }
         return true;
-    }
-
-    private ListNode reverse(ListNode head) {
-        ListNode oriNext = null;
-        ListNode next = null;
-
-        while (head != null) {
-            oriNext = head.next;
-            head.next = next;
-            next = head;
-            head = oriNext;
-        }
-        return next;
     }
 }
 // @lc code=end
