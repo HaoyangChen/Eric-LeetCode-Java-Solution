5fb2a74d45343c064aa19fb5ad5a0072c1f8c6c3
Haoyang Chen <haoyangchen0901@gmail.com>
2021-06-25 18:05:06 -0700

    Add Additional Implementation to Question 328

--- 328.odd-even-linked-list.java
+++ 328.odd-even-linked-list.java
@@ -17,27 +17,46 @@
  */
 
  // Shangan Solution
+// class Solution {
+//     public ListNode oddEvenList(ListNode head) {
+//         ListNode oddDummy = new ListNode(-1);
+//         ListNode evenDummy = new ListNode(-1);
+//         ListNode oddIterator = oddDummy;
+//         ListNode evenIterator = evenDummy;
+//         while (head != null) {
+//             oddIterator.next = head;
+//             oddIterator = oddIterator.next;
+//             head = head.next;
+//             if (head == null) {
+//                 break;
+//             }
+//             evenIterator.next = head;
+//             evenIterator = evenIterator.next;
+//             head = head.next;
+//         }
+//         oddIterator.next = evenDummy.next;
+//         evenIterator.next = null;
+//         return oddDummy.next;
+//     }
+// }
+
+// Time Complexity: O(n) - there are total n nodes and we viist each node once
+// Space Complexity: O(1) - all we need is the four pointers
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        ListNode oddDummy = new ListNode(-1);
-        ListNode evenDummy = new ListNode(-1);
-        ListNode oddIterator = oddDummy;
-        ListNode evenIterator = evenDummy;
-        while (head != null) {
-            oddIterator.next = head;
-            oddIterator = oddIterator.next;
-            head = head.next;
-            if (head == null) {
-                break;
-            }
-            evenIterator.next = head;
-            evenIterator = evenIterator.next;
-            head = head.next;
+        if (head == null) return null;
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
         }
-        oddIterator.next = evenDummy.next;
-        evenIterator.next = null;
-        return oddDummy.next;
+        odd.next = evenHead;
+        return head;
     }
 }
+
+
 // @lc code=end
 
