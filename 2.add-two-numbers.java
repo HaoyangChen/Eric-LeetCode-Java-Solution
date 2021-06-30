13d32a6f943492dbfccf6160e232010bad388def
Haoyang Chen <haoyangchen0901@gmail.com>
2021-06-29 23:27:40 -0700

    Complete Question 13

---
+++ 2.add-two-numbers.java
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=2 lang=java
+ *
+ * [2] Add Two Numbers
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+ // Time Complexity: O(max(m, n)). Assume that m and n represents the length of l1 and l2 respectively. The algorithm iterates at most max(m, n) times
+ // Space Complexity: O(max(m, n)). The length of the new list is at most max(m, n) + 1
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1 , q = l2, curr = dummyHead;
+        int carry = 0;
+
+        while (p != null || q != null) {
+            int x = (p != null) ?  p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            
+            int sum = x + y + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+}
+// @lc code=end
+
