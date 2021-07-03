0f2da09fa67dbff4516b1d830dced610b78ecb2d
Haoyang Chen <haoyangchen0901@gmail.com>
2021-07-03 11:43:11 -0700

    Complete Question 138 with Iterative Approach

--- 138.copy-list-with-random-pointer.java
+++ 138.copy-list-with-random-pointer.java
@@ -66,20 +66,54 @@ class Node {
 
 
 
-// Method: Recursive Aprroach
+// Method 1: Recursive Aprroach
 // Time Complexity: O(N) where N is the number of nodes in the linked list
 // Space Complexity: O(N). If we look closely, we have the recursion stack and we also have the space complexity to keep track of nodes already cloned using the visited dictionary. 
-class Solution {
-    HashMap<Node, Node> visitedHash = new HashMap<Node, Node>();
+// class Solution {
+//     HashMap<Node, Node> visitedHash = new HashMap<Node, Node>();
 
+//     public Node copyRandomList(Node head) {
+//         if (head == null) return null;
+//         if (this.visitedHash.containsKey(head)) return this.visitedHash.get(head);
+//         Node node = new Node(head.val, null, null);
+//         this.visitedHash.put(head, node);
+//         node.next = copyRandomList(head.next);
+//         node.random = copyRandomList(head.random);
+//         return node; 
+//     }
+// }
+
+// Method 2: Iterative Apprach with O(N) Space
+// Time Complexity: O(N) because we make one pass over the original linked list
+// Space Complexity: O(N) as we have a dictionary containing mapping from old list nodes to new list nodes. Since there are N nodes, we have O(N) space complexity
+class Solution {
+    HashMap<Node, Node> visited = new HashMap<Node, Node>();
     public Node copyRandomList(Node head) {
         if (head == null) return null;
-        if (this.visitedHash.containsKey(head)) return this.visitedHash.get(head);
-        Node node = new Node(head.val, null, null);
-        this.visitedHash.put(head, node);
-        node.next = copyRandomList(head.next);
-        node.random = copyRandomList(head.random);
-        return node; 
+        Node oldNode = head;
+        Node newNode = new Node(oldNode.val);
+        this.visited.put(oldNode, newNode);
+
+        while (oldNode != null) {
+            newNode.random = this.getClonedNode(oldNode.random);
+            newNode.next = this.getClonedNode(oldNode.next);
+
+            oldNode = oldNode.next;
+            newNode = newNode.next;
+        }
+        return this.visited.get(head);
+    }
+
+    public Node getClonedNode(Node node) {
+        if (node != null) {
+            if (this.visited.containsKey(node)) {
+                return this.visited.get(node);
+            } else {
+                this.visited.put(node, new Node(node.val, null, null));
+                return this.visited.get(node);
+            }
+        }
+        return null;
     }
 }
 // @lc code=end
