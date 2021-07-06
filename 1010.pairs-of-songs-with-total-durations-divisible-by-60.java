8f907608565c87d62985acb02bf7970fafc72660
Haoyang Chen <haoyangchen0901@gmail.com>
2021-07-05 23:27:53 -0700

    Adjust Question 1010

--- 1010.pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010.pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -5,15 +5,18 @@
  */
 
 // @lc code=start
+
+// Time Complexity: O(n) when n is the length of the input array because we would visit each element in time once
+// Space Complexity: O(1) because the size of the array remainders is fixed with 60
 class Solution {
     public int numPairsDivisibleBy60(int[] time) {
         int remainders[] = new int[60];
         int count = 0;
         for (int t: time) {
             if (t % 60 == 0) {
-                count += remainder[0];
+                count += remainders[0];
             } else {
-                count += remainder[60 - t % 60];
+                count += remainders[60 - t % 60];
             }
             remainders[t % 60]++;
         }
