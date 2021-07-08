0e3c7f47e1b9419297db97137bdd68efeb084cf5
Haoyang Chen <haoyangchen0901@gmail.com>
2021-07-07 23:35:53 -0700

    Complete Question 979

---
+++ 979.distribute-coins-in-binary-tree.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=979 lang=java
+ *
+ * [979] Distribute Coins in Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ans;
+    public int distributeCoins(TreeNode root) {
+        ans = 0;
+        dfs(root);
+        return ans;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) return 0;
+        int L = dfs(node.left);
+        int R = dfs(node.right);
+        ans += Math.abs(L) + Math.abs(R);
+        return node.val + L + R - 1;
+    }
+}
+// @lc code=end
+
