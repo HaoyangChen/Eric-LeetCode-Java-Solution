0ece5a8989d57c0a56b1768dab1fd4e261ae4018
Haoyang Chen <haoyangchen0901@gmail.com>
2021-07-18 16:31:18 -0700

    Complete Question 202 with Floyd's Cycle-Finding Algorithm

--- 202.happy-number.java
+++ 202.happy-number.java
@@ -9,11 +9,36 @@
 // Method 1: Detect Cycle with Hash Set
 // Time Complexity: O(logn) - finding the next value for a given number has a cost of O(logn) because we're processing each digit in the number
 // Space Complexity: O(logn) 
+// class Solution {
+//     private int getNext(int n) {
+//         int totalSum = 0;
+//         while (n > 0) {
+//             int d= n % 10;
+//             n = n / 10;
+//             totalSum += d * d;
+//         }
+//         return totalSum;
+//     }
+
+//     public boolean isHappy(int n) {
+//         Set<Integer> seen = new HashSet<>();
+//         while (n != 1 && !seen.contains(n)) {
+//             seen.add(n);
+//             n = getNext(n);
+//         }
+//         return n == 1;
+//     }
+// }
+
+
+// Method 2:  Floyd's Cycle-Finding Algorithm
+// Time Complexity: O(logn)
+// Space Complexity: O(1) we don't need a hashset to detect the cycle. The pointers require constant extra space
 class Solution {
-    private int getNext(int n) {
+    public int getNext(int n) {
         int totalSum = 0;
         while (n > 0) {
-            int d= n % 10;
+            int d = n % 10;
             n = n / 10;
             totalSum += d * d;
         }
@@ -21,13 +46,15 @@ class Solution {
     }
 
     public boolean isHappy(int n) {
-        Set<Integer> seen = new HashSet<>();
-        while (n != 1 && !seen.contains(n)) {
-            seen.add(n);
-            n = getNext(n);
+        int slowRunner = n;
+        int fastRunner = getNext(n);
+        while (fastRunner != 1 && slowRunner != fastRunner) {
+            slowRunner = getNext(slowRunner);
+            fastRunner = getNext(getNext(fastRunner));
         }
-        return n == 1;
+        return fastRunner == 1;
     }
 }
+
 // @lc code=end
 
