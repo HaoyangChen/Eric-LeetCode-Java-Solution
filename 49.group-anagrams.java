253f13d9751a256af0bb379cc4a15af2253eaf00
Haoyang Chen <haoyangchen0901@gmail.com>
2021-07-23 23:33:41 -0700

    Complete Question 49 with Sorted String

--- 49.group-anagrams.java
+++ 49.group-anagrams.java
@@ -5,9 +5,21 @@
  */
 
 // @lc code=start
+
+// Time Complexity: O(NKlogK), where N is the length of strs, and K is the maximum length of a string in strs. The outer loop has complexity O(N) as we iterate through each string. Then, we sort each string in O(KlogK) time
+// Space Complexity: O(NK) the total information content stored in ans
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        for (String str: strs) {
+            char[] characters = str.toCharArray();
+            Arrays.sort(characters);
+            String key = String.valueOf(characters);
+            if(!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(str);
+        }
+        return new ArrayList(ans.values());
     }
 }
 // @lc code=end
