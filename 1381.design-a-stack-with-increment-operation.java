c762eb716233d1ba8d0433ec87a0f416171fa2f7
Haoyang Chen <haoyangchen0901@gmail.com>
2021-07-24 16:39:00 -0700

    Optimize Question 1381

--- 1381.design-a-stack-with-increment-operation.java
+++ 1381.design-a-stack-with-increment-operation.java
@@ -7,12 +7,46 @@
 // @lc code=start
 
 // Method 1:
+// class CustomStack {
+//     int[] stack;
+//     int top;
+
+//     public CustomStack(int maxSize) {
+//         stack = new int[maxSize];
+//         top = -1;
+//     }
+    
+//     public void push(int x) {
+//         if (top != stack.length - 1) {
+//             ++top;
+//             stack[top] = x;
+//         }
+//     }
+    
+//     public int pop() {
+//         if (top == -1) return -1;
+//         --top;
+//         return stack[top + 1];
+//     }
+    
+//     public void increment(int k, int val) {
+//         int limit = Math.min(k, top + 1);
+//         for (int i = 0; i < limit; ++i) {
+//             stack[i] += val;
+//         }
+//     }
+// }
+
+
+// Method 2: with optimization
 class CustomStack {
     int[] stack;
+    int[] add;
     int top;
 
     public CustomStack(int maxSize) {
         stack = new int[maxSize];
+        add = new int[maxSize];
         top = -1;
     }
     
@@ -25,17 +59,23 @@ class CustomStack {
     
     public int pop() {
         if (top == -1) return -1;
+        int result = stack[top] + add[top];
+        if (top != 0) {
+            add[top - 1] += add[top];
+        }
+        add[top] = 0;
         --top;
-        return stack[top + 1];
+        return result;
     }
     
     public void increment(int k, int val) {
-        int limit = Math.min(k, top + 1);
-        for (int i = 0; i < limit; ++i) {
-            stack[i] += val;
+        int limit = Math.min(k - 1, top);
+        if (limit >= 0) {
+            add[limit] += val;
         }
     }
 }
+
 
 /**
  * Your CustomStack object will be instantiated and called as such:
