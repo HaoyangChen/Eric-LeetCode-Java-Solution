8ca1e3ac1f79cf930b444ca890a61fd3e2b95791
Haoyang Chen <haoyangchen0901@gmail.com>
2021-07-27 23:53:06 -0700

    Add Additional Method to Question 652

--- 652.find-duplicate-subtrees.java
+++ 652.find-duplicate-subtrees.java
@@ -20,28 +20,56 @@
  *     }
  * }
  */
+// class Solution {
+//     private HashMap<String, Integer> memo = new HashMap<>();
+//     private LinkedList<TreeNode> res = new LinkedList<>();  
+//     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+//         traverse(root);
+//         return res;
+//     }
+
+//     private String traverse(TreeNode root) {
+//         if (root == null) {
+//             return "#";
+//         }
+//         String left = traverse(root.left);
+//         String right = traverse(root.right);
+//         String subtree = left + "," + right + "," + root.val;
+//         int freq = memo.getOrDefault(subtree, 0);
+//         if (freq == 1) {
+//             res.add(root);
+//         }
+//         memo.put(subtree, freq + 1);
+//         return subtree;
+//     }
+// }
+
+// Method 2
 class Solution {
-    private HashMap<String, Integer> memo = new HashMap<>();
-    private LinkedList<TreeNode> res = new LinkedList<>();  
+    int t;
+    Map<String, Integer> trees;
+    Map<Integer, Integer> count;
+    List<TreeNode> ans;
+
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        traverse(root);
-        return res;
+        t = 1;
+        trees = new HashMap();
+        count = new HashMap();
+        ans = new ArrayList();
+        lookup(root);
+        return ans;
     }
 
-    private String traverse(TreeNode root) {
-        if (root == null) {
-            return "#";
-        }
-        String left = traverse(root.left);
-        String right = traverse(root.right);
-        String subtree = left + "," + right + "," + root.val;
-        int freq = memo.getOrDefault(subtree, 0);
-        if (freq == 1) {
-            res.add(root);
-        }
-        memo.put(subtree, freq + 1);
-        return subtree;
+    public int lookup(TreeNode node) {
+        if (node == null) return 0;
+        String serial = node.val + "," + lookup(node.left) + "," + lookup(node.right);
+        int uid = trees.computeIfAbsent(serial, x-> t++);
+        count.put(uid, count.getOrDefault(uid, 0) + 1);
+        if (count.get(uid) == 2)
+            ans.add(node);
+        return uid;
     }
 }
+
 // @lc code=end
 
