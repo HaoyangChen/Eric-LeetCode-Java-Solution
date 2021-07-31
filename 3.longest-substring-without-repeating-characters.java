e9784459579f5e41c7cb72cc0575890df374e82b
Haoyang Chen <haoyangchen0901@gmail.com>
2021-07-30 23:52:53 -0700

    Add Sliding Window Method to Question 3

--- 3.longest-substring-without-repeating-characters.java
+++ 3.longest-substring-without-repeating-characters.java
@@ -5,23 +5,50 @@
  */
 
 // @lc code=start
-class Solution {
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int max = 0;
+//         int count = 0;
+//         Set<Character> visited = new HashSet<>();
+//         int len = s.length();
+//         for (int i = 0; i < len; i++) {
+//             char c = s.charAt(i);
+//             while (visited.contains(c)) {
+//                 visited.remove(s.charAt(i - count));
+//                 count--;
+//             }
+//             visited.add(c);
+//             count++;
+//             max = Math.max(max, count);
+//         }
+//         return max;
+//     }
+// }
+
+// Method 2: Sliding Window Method
+public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int max = 0;
-        int count = 0;
-        Set<Character> visited = new HashSet<>();
-        int len = s.length();
-        for (int i = 0; i < len; i++) {
-            char c = s.charAt(i);
-            while (visited.contains(c)) {
-                visited.remove(s.charAt(i - count));
-                count--;
+        int[] chars = new int[128];
+
+        int left = 0;
+        int right = 0;
+
+        int res = 0;
+        while (right < s.length()) {
+            char r = s.charAt(right);
+            chars[r]++;
+
+            while (chars[r] > 1) {
+                char l = s.charAt(left);
+                chars[l]--;
+                left++;
             }
-            visited.add(c);
-            count++;
-            max = Math.max(max, count);
+
+            res = Math.max(res, right - left + 1);
+
+            right++;
         }
-        return max;
+        return res;
     }
 }
 // @lc code=end
