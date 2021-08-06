e0567a95e94bcedf85c0b1afe30b8ba6a4aa5670
Haoyang Chen <haoyangchen0901@gmail.com>
2021-08-05 23:40:27 -0700

    Add Sliding Window Optimized Method

--- 3.longest-substring-without-repeating-characters.java
+++ 3.longest-substring-without-repeating-characters.java
@@ -26,30 +26,48 @@
 // }
 
 // Method 2: Sliding Window Method
-public class Solution {
+// public class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int[] chars = new int[128];
+
+//         int left = 0;
+//         int right = 0;
+
+//         int res = 0;
+//         while (right < s.length()) {
+//             char r = s.charAt(right);
+//             chars[r]++;
+
+//             while (chars[r] > 1) {
+//                 char l = s.charAt(left);
+//                 chars[l]--;
+//                 left++;
+//             }
+
+//             res = Math.max(res, right - left + 1);
+
+//             right++;
+//         }
+//         return res;
+//     }
+// }
+
+
+// Method 3: Sliding Window Optimized
+class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int[] chars = new int[128];
-
-        int left = 0;
-        int right = 0;
-
-        int res = 0;
-        while (right < s.length()) {
-            char r = s.charAt(right);
-            chars[r]++;
-
-            while (chars[r] > 1) {
-                char l = s.charAt(left);
-                chars[l]--;
-                left++;
+        int length = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int j = 0, i = 0; j < length; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
             }
-
-            res = Math.max(res, right - left + 1);
-
-            right++;
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
         }
-        return res;
+        return ans;
     }
 }
+
 // @lc code=end
 
