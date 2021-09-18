106404147d0dcffc1621e298a014164d5c744bcd
Haoyang Chen <haoyangchen0901@gmail.com>
2021-09-17 23:59:49 -0700

    complete question 64

--- 64.minimum-path-sum.java
+++ 64.minimum-path-sum.java
@@ -5,9 +5,14 @@
  */
 
 // @lc code=start
-class Solution {
+public class Solution {
+    public int calculate(int[][] grid, int i, int j) {
+        if (i == grid.length || j == grid[0].length) return Integer.MAX_VALUE;
+        if (i == grid.length - 1 && j == grid[0].length - 1) return grid[i][j];
+        return grid[i][j] + Math.min(calculate(grid, i + 1, j), calculate(grid, i, j + 1));
+    }
     public int minPathSum(int[][] grid) {
-        
+        return calculate(grid, 0, 0);
     }
 }
 // @lc code=end
