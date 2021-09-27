ef5f50080ad13b93ffda62a0515f9db49fb3086c
Haoyang Chen <haoyangchen0901@gmail.com>
2021-09-26 23:04:59 -0700

    Complete Question 165 with Two Pass Split Parse

--- 165.compare-version-numbers.java
+++ 165.compare-version-numbers.java
@@ -5,9 +5,24 @@
  */
 
 // @lc code=start
+
+// Method 1: Split + Parse, Two Pass
+// Time Complexity: O(N + M + max(N, M)), where N and M are lengths of input strings
+// Space Complexity: O(N + M) tp store arrays num1 and nums2
 class Solution {
     public int compareVersion(String version1, String version2) {
-        
+        String[] nums1 = version1.split("\\.");
+        String[] nums2 = version2.split("\\.");
+        int n1 = nums1.length, n2 = nums2.length;
+        int i1, i2;
+        for (int i = 0; i < Math.max(n1, n2); i++) {
+            i1 = i < n1 ? Integer.parseInt(nums1[i]) : 0;
+            i2 = i < n2 ? Integer.parseInt(nums2[i]) : 0;
+            if (i1 != i2) {
+                return i1 < i2 ? -1 : 1;
+            }
+        }
+        return 0;
     }
 }
 // @lc code=end
