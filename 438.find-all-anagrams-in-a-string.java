380bba9e7876cfc74a901b7624f8ebaf2962d944
Haoyang Chen <haoyangchen0901@gmail.com>
2021-10-15 23:57:13 -0700

    Start Question 438

--- 438.find-all-anagrams-in-a-string.java
+++ 438.find-all-anagrams-in-a-string.java
@@ -5,38 +5,48 @@
  */
 
 // @lc code=start
+// class Solution {
+//     public List<Integer> findAnagrams(String s, String p) {
+//         HashMap<Character, Integer> need = new HashMap<Character, Integer>();
+//         HashMap<Character, Integer> window = new HashMap<Character, Integer>();
+//         for (char c: p.toCharArray()) need.put(c, need.getOrDefault(c, 0) + 1);
+//         int left = 0, right = 0;
+//         int valid = 0;
+//         List<Integer> resultList = new ArrayList<>();
+//         while (right < s.length()) {
+//             char c = s.charAt(right);
+//             right++;
+//             if (need.containsKey(c)) {
+//                 window.put(c, window.getOrDefault(c, 0) + 1);
+//                 if (window.get(c).equals(need.get(c))) {
+//                     valid++;
+//                 }
+//             }
+//             while (right - left >= p.length()) {
+//                 if (valid == need.size()) {
+//                     resultList.add(left);
+//                 }
+//                 char c1 = s.charAt(left);
+//                 left++;
+//                 if (need.containsKey(c1)) {
+//                     if (window.get(c1).equals(need.get(c1))) {
+//                         valid--;
+//                     }
+//                     window.put(c1, window.getOrDefault(c1, 0) - 1);
+//                 }
+//             }
+//         }
+//         return resultList;
+//     }
+// }
+
+
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        HashMap<Character, Integer> need = new HashMap<Character, Integer>();
-        HashMap<Character, Integer> window = new HashMap<Character, Integer>();
-        for (char c: p.toCharArray()) need.put(c, need.getOrDefault(c, 0) + 1);
-        int left = 0, right = 0;
-        int valid = 0;
-        List<Integer> resultList = new ArrayList<>();
-        while (right < s.length()) {
-            char c = s.charAt(right);
-            right++;
-            if (need.containsKey(c)) {
-                window.put(c, window.getOrDefault(c, 0) + 1);
-                if (window.get(c).equals(need.get(c))) {
-                    valid++;
-                }
-            }
-            while (right - left >= p.length()) {
-                if (valid == need.size()) {
-                    resultList.add(left);
-                }
-                char c1 = s.charAt(left);
-                left++;
-                if (need.containsKey(c1)) {
-                    if (window.get(c1).equals(need.get(c1))) {
-                        valid--;
-                    }
-                    window.put(c1, window.getOrDefault(c1, 0) - 1);
-                }
-            }
-        }
-        return resultList;
+        int sLength = s.length(), pLength = p.length();
+        List<Integer> result = new ArrayList<>();
+        if (sLength < pLength) return result;
+        
     }
 }
 // @lc code=end
