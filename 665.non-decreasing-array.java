5c3010613551b70db33a42d2c1ea8929ae041ad1
Haoyang Chen <haoyangchen0901@gmail.com>
2021-10-30 23:56:23 -0700

    Start Question 685

---
+++ 665.non-decreasing-array.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=665 lang=java
+ *
+ * [665] Non-decreasing Array
+ */
+
+// @lc code=start
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
