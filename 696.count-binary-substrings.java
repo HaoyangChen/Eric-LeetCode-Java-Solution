a71491133a42792ab668445b0574cd3281d81579
Haoyang Chen <haoyangchen0901@gmail.com>
2021-10-31 23:51:32 -0700

    Complete Question 696

---
+++ 696.count-binary-substrings.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=696 lang=java
+ *
+ * [696] Count Binary Substrings
+ */
+
+// @lc code=start
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int ans = 0, prev = 0, cur = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i - 1) != s.charAt(i)) {
+                ans += Math.min(prev, cur);
+                prev = cur;
+                cur = 1;
+            } else {
+                cur++;
+            }
+        }
+        return ans + Math.min(prev, cur);
+    }
+}
+// @lc code=end
+
