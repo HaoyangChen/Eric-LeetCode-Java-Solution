0af574fdf006ddcaf60ecb473eed6221a0173ca4
Haoyang Chen <haoyangchen0901@gmail.com>
2021-11-13 15:42:55 -0800

    Complete Question 27 with Two Pointers Implementation

--- 27.remove-element.java
+++ 27.remove-element.java
@@ -5,9 +5,19 @@
  */
 
 // @lc code=start
+
+// Time Complexity: O(n). Assume the array has a total of n elements, both i and j traverse at most 2n steps.
+// Space Complexity: O(1)
 class Solution {
     public int removeElement(int[] nums, int val) {
-        
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
     }
 }
 // @lc code=end
