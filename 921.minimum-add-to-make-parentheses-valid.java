794896f6330424fb30b71ec77e7a39e1b6cb87a2
Haoyang Chen <haoyangchen0901@gmail.com>
2021-11-15 01:26:22 -0800

    Complete Question 921

--- 921.minimum-add-to-make-parentheses-valid.java
+++ 921.minimum-add-to-make-parentheses-valid.java
@@ -5,9 +5,20 @@
  */
 
 // @lc code=start
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 class Solution {
     public int minAddToMakeValid(String s) {
-        
+        int answer = 0, balance = 0;
+        for (int i = 0; i < s.length(); i++) {
+            balance += s.charAt(i) == '(' ? 1 : -1;
+            if (balance == -1) {
+                balance++;
+                answer++;
+            }
+        }
+        return answer + balance;
     }
 }
 // @lc code=end
