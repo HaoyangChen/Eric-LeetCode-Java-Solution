f7e690878a6003f3e082f7804b29a02c625e683c
Haoyang Chen <haoyangchen0901@gmail.com>
2021-11-18 23:54:02 -0800

    Add Question 423

---
+++ 423.reconstruct-original-digits-from-english.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=423 lang=java
+ *
+ * [423] Reconstruct Original Digits from English
+ */
+
+// @lc code=start
+class Solution {
+    public String originalDigits(String s) {
+        
+    }
+}
+// @lc code=end
+
