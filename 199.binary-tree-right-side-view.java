16c01d88cf447d0aed0112015b5055017998eea3
Haoyang Chen <haoyangchen0901@gmail.com>
2021-11-23 16:27:15 -0800

    Add One Queue BFS Implementation to Question 199

--- 199.binary-tree-right-side-view.java
+++ 199.binary-tree-right-side-view.java
@@ -57,24 +57,52 @@
 // Method 2: Recursive DFS
 // Time Complexity: O(N) since one has to visit each node
 // Space Complexity: O(H) to keep the recursion stack, where H is the tree height. In the worst-case situation is a skewed tree, when H = N
-class Solution {
-    List<Integer> result = new ArrayList();
-    public List<Integer> rightSideView(TreeNode root) {
-        if (root == null) return result;
-        helper(root, 0);
-        return result;
-    }
+// class Solution {
+//     List<Integer> result = new ArrayList();
+//     public List<Integer> rightSideView(TreeNode root) {
+//         if (root == null) return result;
+//         helper(root, 0);
+//         return result;
+//     }
     
-    private void helper(TreeNode node, int level) {
-        if (level == result.size()) {
-            result.add(node.val);
+//     private void helper(TreeNode node, int level) {
+//         if (level == result.size()) {
+//             result.add(node.val);
+//         }
+//         if (node.right != null) {
+//             helper(node.right, level + 1);
+//         }
+//         if (node.left != null) {
+//             helper(node.left, level + 1);
+//         }
+//     }
+// }
+
+
+// BFS: One Queue + Level Size Measurements
+// Time Complexity: O(N) since each node has to be visited
+// Space Complexity: O(D) to keep the queues, where D is a tree's diameter. The level can contain up to N/2 tree nodes in the case of a complete binary tree.
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        ArrayDeque<TreeNode> queue = new ArrayDeque(){{ offer(root); }};
+        List<Integer> result = new ArrayList<>();
+        while(!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                if (i == levelSize - 1) {
+                    result.add(node.val);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
         }
-        if (node.right != null) {
-            helper(node.right, level + 1);
-        }
-        if (node.left != null) {
-            helper(node.left, level + 1);
-        }
+        return result;
     }
 }
 // @lc code=end
