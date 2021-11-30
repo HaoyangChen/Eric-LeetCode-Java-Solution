482ba0b5cb297fa1c26eb61900a0fa2eaa098306
Haoyang Chen <haoyangchen0901@gmail.com>
2021-11-29 23:56:12 -0800

    Edit Question 721

--- 721.accounts-merge.java
+++ 721.accounts-merge.java
@@ -6,9 +6,12 @@
 
 // @lc code=start
 class Solution {
-    public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        HashSet<String> visited = new HashSet<>();
+    HashSet<String> visited = new HashSet<>();
         Map<String, List<String>> adjacent = new HashMap<String, List<String>>();
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        visited.add(email);
+        // Add the email vector that contains the current component's emails
+        mergedAccount.add(email);
     }
 }
 // @lc code=end
