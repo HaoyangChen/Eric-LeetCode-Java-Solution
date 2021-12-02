682e7446ca9fda6ad27a408715515eb1259d9116
Haoyang Chen <haoyangchen0901@gmail.com>
2021-12-01 23:59:33 -0800

    Add Question 1103

---
+++ 1103.distribute-candies-to-people.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1103 lang=java
+ *
+ * [1103] Distribute Candies to People
+ */
+
+// @lc code=start
+class Solution {
+    public int[] distributeCandies(int candies, int num_people) {
+        
+    }
+}
+// @lc code=end
+
