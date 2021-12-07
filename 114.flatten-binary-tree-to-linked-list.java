df91ef153b8fc44972685cc4968ef75711b4ee88
Haoyang Chen <haoyangchen0901@gmail.com>
2021-12-06 18:35:02 -0800

    Complete Question 114

--- 114.flatten-binary-tree-to-linked-list.java
+++ 114.flatten-binary-tree-to-linked-list.java
@@ -20,23 +20,45 @@
  *     }
  * }
  */
+// class Solution {
+//     public void flatten(TreeNode root) {
+//         if (root == null) return;
+//         flatten(root.left);
+//         flatten(root.right);
+
+//         TreeNode left = root.left;
+//         TreeNode right = root.right;
+
+//         root.left = null;
+//         root.right = left;
+
+//         TreeNode p = root;
+//         while (p.right != null) {
+//             p = p.right;
+//         }
+//         p.right = right;
+//     }
+// }
+
+// O(1) space iterative solution
+// Time Complexity: O(N) since we process each node of the tree at most twice
+// Spcae Complexity: O(1)
 class Solution {
     public void flatten(TreeNode root) {
         if (root == null) return;
-        flatten(root.left);
-        flatten(root.right);
-
-        TreeNode left = root.left;
-        TreeNode right = root.right;
-
-        root.left = null;
-        root.right = left;
-
-        TreeNode p = root;
-        while (p.right != null) {
-            p = p.right;
+        TreeNode node = root;
+        while (node != null) {
+            if (node.left != null) {
+                TreeNode rightMost = node.left;
+                while (rightMost.right != null) {
+                    rightMost = rightMost.right;
+                }
+                rightMost.right = node.right;
+                node.right = node.left;
+                node.left = null;
+            }
+            node = node.right;
         }
-        p.right = right;
     }
 }
 // @lc code=end
