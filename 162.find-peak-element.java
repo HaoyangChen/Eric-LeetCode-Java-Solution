7860002e3316e63dafe09c5b9b1ff2a9bb540f15
Haoyang Chen <haoyangchen0901@gmail.com>
2021-12-13 23:57:52 -0800

    Add Linear Scan Implementation to Question 162

--- 162.find-peak-element.java
+++ 162.find-peak-element.java
@@ -5,6 +5,18 @@
  */
 
 // @lc code=start
+
+// public class Solution {
+//     public int findPeakElement(int[] nums) {
+//         for (int i = 0; i < nums.length - 1; i++) {
+//             if (nums[i] > nums[i + 1])
+//                 return i;
+//         }
+//         return nums.length - 1;
+//     }
+// }
+
+
 class Solution {
     public int findPeakElement(int[] nums) {
         if (nums.length == 0 || nums == null) {
