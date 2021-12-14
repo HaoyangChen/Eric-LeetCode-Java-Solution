e9f6226157273f689a181b54f0d4b8afaa52a1dc
Haoyang Chen <haoyangchen0901@gmail.com>
2021-12-14 00:33:40 -0800

    Complete Question 162 with Iterative Bianry Search

--- 162.find-peak-element.java
+++ 162.find-peak-element.java
@@ -17,22 +17,37 @@
 // }
 
 
+// class Solution {
+//     public int findPeakElement(int[] nums) {
+//         if (nums.length == 0 || nums == null) {
+//             return -1;
+//         }
+//         int left = 0;
+//         int right = nums.length - 1;
+//         while (left + 1 < right) {
+//             int mid = left + (right - left) / 2;
+//             if (nums[mid] > nums[mid - 1]) {
+//                 left = mid;
+//             } else {
+//                 right = mid;
+//             }
+//         }
+//         return nums[left] > nums[right] ?  left : right;
+//     }
+// }
+
+// Iterative Binary Search
 class Solution {
     public int findPeakElement(int[] nums) {
-        if (nums.length == 0 || nums == null) {
-            return -1;
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
         }
-        int left = 0;
-        int right = nums.length - 1;
-        while (left + 1 < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] > nums[mid - 1]) {
-                left = mid;
-            } else {
-                right = mid;
-            }
-        }
-        return nums[left] > nums[right] ?  left : right;
+        return l;
     }
 }
 // @lc code=end
