af7a973bc4f0d8dc9467ab84ed644c104ec52921
Haoyang Chen <haoyangchen0901@gmail.com>
2021-12-15 11:02:32 -0800

    Complete Question 227 without Stack

---
+++ 227.basic-calculator-ii.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=227 lang=java
+ *
+ * [227] Basic Calculator II
+ */
+
+// @lc code=start
+
+// Approach without using stack
+// Time Complexity: O(n) where n is the length of the string s
+// Space Complexity: O(1) as constant extra space is used to store lastNumber and result
+class Solution {
+    public int calculate(String s) {
+        if (s == null || s.isEmpty()) return 0;
+        int length = s.length();
+        int currentNumber = 0, lastNumber = 0, result = 0;
+        char operation = '+';
+        for (int i = 0; i < length; i++) {
+            char currentChar = s.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == length - 1) {
+                if (operation == '+' || operation == '-') {
+                    result += lastNumber;
+                    lastNumber = (operation == '+') ? currentNumber : -currentNumber;
+                } else if (operation == '*') {
+                    lastNumber = lastNumber * currentNumber;
+                } else if (operation == '/') {
+                    lastNumber = lastNumber / currentNumber;
+                }
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        result += lastNumber;
+        return result;
+    }
+}
+// @lc code=end
+
