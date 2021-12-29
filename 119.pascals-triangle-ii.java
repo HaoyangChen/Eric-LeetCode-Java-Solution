68202ee159f70cb50f03ff2f71b5507a07ef799c
Haoyang Chen <haoyangchen0901@gmail.com>
2021-12-29 15:06:54 -0800

    Complete Question 119 with DP

--- 119.pascals-triangle-ii.java
+++ 119.pascals-triangle-ii.java
@@ -7,20 +7,38 @@
 // @lc code=start
 
 // Method 1: Brute Force Recursion
+// class Solution {
+//     public List<Integer> getRow(int rowIndex) {
+//         List<Integer> ans = new ArrayList<>();
+//         for (int i = 0; i <= rowIndex; i++) {
+//             ans.add(getNum(rowIndex, i));
+//         }
+//         return ans;
+//     }
+
+//     private int getNum(int row, int col) {
+//         if (row == 0 || col == 0 || row == col) {
+//             return 1;
+//         }
+//         return getNum(row - 1, col - 1) + getNum(row - 1, col);
+//     }
+// }
+
+// Method 2: Memory Efficient Dynamic Programming
+// Time Complexity: O(k^2)
+// Space Complexity: O(k) - no extra space is used other than that required to hold the output
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i <= rowIndex; i++) {
-            ans.add(getNum(rowIndex, i));
+        List<Integer> row = new ArrayList<>(rowIndex + 1) {
+            {add(1);};
+        };
+        for (int i = 0; i < rowIndex; i++) {
+            for (int j = i; j > 0; j--) {
+                row.set(j, row.get(j) + row.get(j - 1));
+            }
+            row.add(1);
         }
-        return ans;
-    }
-
-    private int getNum(int row, int col) {
-        if (row == 0 || col == 0 || row == col) {
-            return 1;
-        }
-        return getNum(row - 1, col - 1) + getNum(row - 1, col);
+        return row;
     }
 }
 // @lc code=end
