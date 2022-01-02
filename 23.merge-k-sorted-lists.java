e51e3bbed7ac0af18d759d73b477fd2345abfdbe
Haoyang Chen <haoyangchen0901@gmail.com>
2022-01-02 00:20:21 -0800

    Complete Question 23

--- 23.merge-k-sorted-lists.java
+++ 23.merge-k-sorted-lists.java
@@ -15,27 +15,68 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-public ListNode mergeKLists(ListNode[] lists) {
-    List<Integer> l = new ArrayList<Integer>();
+// public ListNode mergeKLists(ListNode[] lists) {
+//     List<Integer> l = new ArrayList<Integer>();
    
-    for (ListNode ln : lists) {
-        while (ln != null) {
-            l.add(ln.val);
-            ln = ln.next;
+//     for (ListNode ln : lists) {
+//         while (ln != null) {
+//             l.add(ln.val);
+//             ln = ln.next;
+//         }
+//     }
+   
+//     Collections.sort(l);
+ 
+//     ListNode head = new ListNode(0);
+//     ListNode h = head;
+//     for (int i : l) {
+//         ListNode t = new ListNode(i);
+//         h.next = t;
+//         h = h.next;
+//     }
+//     h.next = null;
+//     return head.next;
+// }
+
+
+// Method: Priority Queue
+// Time Complexity: O(NlogK) where k is the number of linked lists
+// Space Complexity: o(n) creating a new linked list costs O(n) space
+class Solution {
+    class Status implements Comparable<Status> {
+        int val;
+        ListNode pointer;
+        
+        Status(int val, ListNode pointer) {
+            this.val = val;
+            this.pointer = pointer;
+        }
+        
+        public int compareTo(Status status2) {
+            return this.val - status2.val;
         }
     }
-   
-    Collections.sort(l);
- 
-    ListNode head = new ListNode(0);
-    ListNode h = head;
-    for (int i : l) {
-        ListNode t = new ListNode(i);
-        h.next = t;
-        h = h.next;
+    
+    PriorityQueue<Status> queue = new PriorityQueue<>();
+    
+    public ListNode mergeKLists(ListNode[] lists) {
+        for (ListNode node: lists) {
+            if (node != null) {
+                queue.offer(new Status(node.val, node));
+            }
+        }
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+        while (!queue.isEmpty()) {
+            Status f = queue.poll();
+            tail.next = f.pointer;
+            tail = tail.next;
+            if (f.pointer.next != null) {
+                queue.offer(new Status(f.pointer.next.val, f.pointer.next));
+            }
+        }
+        return head.next;
     }
-    h.next = null;
-    return head.next;
 }
 // @lc code=end
 
