934f2ba7fd30da6c48c153e4aea8d431c3cfb36b
Haoyang Chen <haoyangchen0901@gmail.com>
2022-01-06 16:13:42 -0800

    Complete Question 1539 with Binary Search

--- 1539.kth-missing-positive-number.java
+++ 1539.kth-missing-positive-number.java
@@ -5,9 +5,21 @@
  */
 
 // @lc code=start
+
+// Time Complexity: o(logN) where N is the number of elements in the input arrray
+// Space Complexity: O(1) since we don't allocate any addtional data structure
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int pivot = left + (right - left) / 2;
+            if (arr[pivot] - pivot - 1 < k) {
+                left = pivot + 1;
+            } else {
+                right = pivot - 1;
+            }
+        }
+        return left + k;
     }
 }
 // @lc code=end
