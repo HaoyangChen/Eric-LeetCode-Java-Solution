e06ce950f33759d8d5f4811c0d50772f3aa0af6d
Haoyang Chen <haoyangchen0901@gmail.com>
2022-04-28 01:28:10 -0700

    Add Question 53 Complexity Analysis

--- 53.maximum-subarray.java
+++ 53.maximum-subarray.java
@@ -5,6 +5,8 @@
  */
 
 // @lc code=start
+// Time Complexity: O(n) where n is the length of the given array nums since we iterate every element of nums exactly once
+// Space Complexity: O(1) only currentMax and largestSum variables are used
 class Solution {
     public int maxSubArray(int[] nums) {
         int currentMax = nums[0];
