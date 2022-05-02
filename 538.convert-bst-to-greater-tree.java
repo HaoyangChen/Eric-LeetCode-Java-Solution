93a03a1098ef96090ff9e30f5f83fb3339866227
Haoyang Chen <haoyangchen0901@gmail.com>
2022-05-02 16:00:36 -0700

    Add Additional Implementation to Question 538

--- 538.convert-bst-to-greater-tree.java
+++ 538.convert-bst-to-greater-tree.java
@@ -20,13 +20,31 @@
  *     }
  * }
  */
+// class Solution {
+//     private int sum = 0;
+//     public TreeNode convertBST(TreeNode root) {
+//         traverse(root);
+//         return root;
+//     }
+
+//     private void traverse(TreeNode root) {
+//         if (root == null) return;
+//         traverse(root.right);
+//         sum += root.val;
+//         root.val = sum;
+//         traverse(root.left);
+//     }
+// }
+
+
 class Solution {
     private int sum = 0;
+    
     public TreeNode convertBST(TreeNode root) {
         traverse(root);
         return root;
     }
-
+    
     private void traverse(TreeNode root) {
         if (root == null) return;
         traverse(root.right);
