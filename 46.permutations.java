1aa3a6313db510144184cb96cdcae106663d8651
Haoyang Chen <haoyangchen0901@gmail.com>
2022-05-02 19:35:33 -0700

    Add Additional Backtracking Implementation to Question 46

--- 46.permutations.java
+++ 46.permutations.java
@@ -61,39 +61,66 @@
 // }
 
 
+// class Solution {
+//     public void backtrack(int n,
+//                           ArrayList<Integer> nums,
+//                           List<List<Integer>> output,
+//                           int first) {
+//       // if all integers are used up
+//       if (first == n)
+//         output.add(new ArrayList<Integer>(nums));
+//       for (int i = first; i < n; i++) {
+//         // place i-th integer first 
+//         // in the current permutation
+//         Collections.swap(nums, first, i);
+//         // use next integers to complete the permutations
+//         backtrack(n, nums, output, first + 1);
+//         // backtrack
+//         Collections.swap(nums, first, i);
+//       }
+//     }
+  
+//     public List<List<Integer>> permute(int[] nums) {
+//       // init output list
+//       List<List<Integer>> output = new LinkedList();
+  
+//       // convert nums into list since the output is a list of lists
+//       ArrayList<Integer> nums_lst = new ArrayList<Integer>();
+//       for (int num : nums)
+//         nums_lst.add(num);
+  
+//       int n = nums.length;
+//       backtrack(n, nums_lst, output, 0);
+//       return output;
+//     }
+//   }
+
+
 class Solution {
-    public void backtrack(int n,
-                          ArrayList<Integer> nums,
-                          List<List<Integer>> output,
-                          int first) {
-      // if all integers are used up
-      if (first == n)
-        output.add(new ArrayList<Integer>(nums));
-      for (int i = first; i < n; i++) {
-        // place i-th integer first 
-        // in the current permutation
-        Collections.swap(nums, first, i);
-        // use next integers to complete the permutations
-        backtrack(n, nums, output, first + 1);
-        // backtrack
-        Collections.swap(nums, first, i);
-      }
-    }
+  List<List<Integer>> res = new LinkedList<>();
   
-    public List<List<Integer>> permute(int[] nums) {
-      // init output list
-      List<List<Integer>> output = new LinkedList();
-  
-      // convert nums into list since the output is a list of lists
-      ArrayList<Integer> nums_lst = new ArrayList<Integer>();
-      for (int num : nums)
-        nums_lst.add(num);
-  
-      int n = nums.length;
-      backtrack(n, nums_lst, output, 0);
-      return output;
-    }
+  public List<List<Integer>> permute(int[] nums) {
+      LinkedList<Integer> track = new LinkedList<>();
+      boolean[] used = new boolean[nums.length];
+      backtrack(nums, track, used);
+      return res;
   }
+  
+  private void backtrack(int[] nums, LinkedList<Integer> track, boolean[] used) {
+      if (track.size() == nums.length) {
+          res.add(new LinkedList(track));
+          return;
+      }
+      for (int i = 0; i < nums.length; i++) {
+          if (used[i]) continue;
+          track.add(nums[i]);
+          used[i] = true;
+          backtrack(nums, track, used);
+          track.removeLast();
+          used[i] = false;
+      }
+  }
+}
 
 
 
