c5d8d47cc644a617a9a3a889b5afd4818d75f69a
Haoyang Chen <haoyangchen0901@gmail.com>
2022-05-04 08:48:04 -0700

    Add Additional Implementation to Question 77

--- 77.combinations.java
+++ 77.combinations.java
@@ -5,26 +5,49 @@
  */
 
 // @lc code=start
+// class Solution {
+//     List<List<Integer>> result = new LinkedList<>();
+//     int n;
+//     int k;
+
+//     public List<List<Integer>> combine(int n, int k) {
+//         this.n = n;
+//         this.k = k;
+//         backtrack(1, new LinkedList<Integer>());
+//         return result;
+//     }
+
+//     private void backtrack(int first, LinkedList<Integer> curr) {
+//         if (curr.size() == k) {
+//             result.add(new LinkedList<>(curr));
+//         }
+//         for (int i = first; i < n + 1; i++) {
+//             curr.add(i);
+//             backtrack(i + 1, curr);
+//             curr.removeLast();
+//         }
+//     }
+// }
+
+
 class Solution {
     List<List<Integer>> result = new LinkedList<>();
-    int n;
-    int k;
-
+    LinkedList<Integer> track = new LinkedList<>();
+    
     public List<List<Integer>> combine(int n, int k) {
-        this.n = n;
-        this.k = k;
-        backtrack(1, new LinkedList<Integer>());
+        backtrack(1, n, k);
         return result;
     }
-
-    private void backtrack(int first, LinkedList<Integer> curr) {
-        if (curr.size() == k) {
-            result.add(new LinkedList<>(curr));
+    
+    private void backtrack(int start, int n, int k) {
+        if (track.size() == k) {
+            result.add(new LinkedList<>(track));
+            return;
         }
-        for (int i = first; i < n + 1; i++) {
-            curr.add(i);
-            backtrack(i + 1, curr);
-            curr.removeLast();
+        for (int i = start; i <= n; i++) {
+            track.addLast(i);
+            backtrack(i + 1, n, k);
+            track.removeLast();
         }
     }
 }
