796eda6ce169c33a62f615aaf1779177558ad2e6
Haoyang Chen <haoyangchen0901@gmail.com>
2022-05-05 12:53:17 -0700

    Add Optimized DP Implementation to Question 198

--- 198.house-robber.java
+++ 198.house-robber.java
@@ -33,19 +33,37 @@
 // Time Complexity: O(N) since we have a loop from N - 2 to 0 and we can use the pre-calculated values of the dp table for calculating the current value in the table, which is a constant time operation
 // Space Complexity: O(N) amount of space used by the table
 
-class Solution {
+// class Solution {
     
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         if (n == 0) return 0;
+//         int[] maxRobbedAmount = new int[nums.length + 1];
+//         maxRobbedAmount[n] = 0;
+//         maxRobbedAmount[n - 1] = nums[n - 1];
+//         for (int i = n - 2; i >= 0; i--) {
+//             maxRobbedAmount[i] = Math.max(maxRobbedAmount[i + 1], maxRobbedAmount[i + 2] + nums[i]);
+//         }
+//         return maxRobbedAmount[0];
+        
+//     }
+// }
+
+// Optimized DP
+// Time Complexity: O(N) since we have a loop from n - 2 to 0, and we use the precalculated values from the dp table to calculate the current value in the table
+// Space Complexity: O(1) since we are not using a table to store the values and instead we are using two variables to calculate the result 
+class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         if (n == 0) return 0;
-        int[] maxRobbedAmount = new int[nums.length + 1];
-        maxRobbedAmount[n] = 0;
-        maxRobbedAmount[n - 1] = nums[n - 1];
+        int robNextPlusOne = 0;
+        int robNext = nums[n - 1];
         for (int i = n - 2; i >= 0; i--) {
-            maxRobbedAmount[i] = Math.max(maxRobbedAmount[i + 1], maxRobbedAmount[i + 2] + nums[i]);
+            int current = Math.max(robNextPlusOne + nums[i], robNext);
+            robNextPlusOne = robNext;
+            robNext = current;
         }
-        return maxRobbedAmount[0];
-        
+        return robNext;
     }
 }
 // @lc code=end
