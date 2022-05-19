d8508624a618f93f6b05db22fc529fb2deb1c394
Haoyang Chen <haoyangchen0901@gmail.com>
2022-05-18 23:40:23 -0700

    Add Question 1328

---
+++ 1328.break-a-palindrome.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1328 lang=java
+ *
+ * [1328] Break a Palindrome
+ */
+
+// @lc code=start
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        
+    }
+}
+// @lc code=end
+
