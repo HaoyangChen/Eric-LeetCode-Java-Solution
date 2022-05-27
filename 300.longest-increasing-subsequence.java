7c6e852312c2d74fd7670efbc49bdd28935c6d7d
Haoyang Chen <haoyangchen0901@gmail.com>
2022-05-27 15:27:43 -0700

    Add DP solution to Question 300

--- 300.longest-increasing-subsequence.java
+++ 300.longest-increasing-subsequence.java
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-// import java.util.*;
+import java.util.*;
 
 // Method 1: Dynamic Programming
 // Time Comlpexity: O(N^2)
@@ -31,27 +31,47 @@
 // Method 2: Binary Search
 // Time Complexity: O(NlogN) binary search uses log(N) time
 // Space Complexity: O(N) when the input is strictly increasing, the sub array will be the same size as the input
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int[] top = new int[nums.length];
+//         int piles = 0;
+//         for (int i = 0; i < nums.length; i++) {
+//             int poker = nums[i];
+//             int left = 0, right = piles;
+//             while (left < right) {
+//                 int mid = left + (right - left) / 2;
+//                 if (top[mid] > poker) {
+//                     right = mid;
+//                 } else if (top[mid] < poker) {
+//                     left = mid + 1;
+//                 } else {
+//                     right = mid;
+//                 }
+//             }
+//             if (left == piles) piles++;
+//             top[left] = poker;
+//         }
+//         return piles;
+//     }
+// }
+
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] top = new int[nums.length];
-        int piles = 0;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
         for (int i = 0; i < nums.length; i++) {
-            int poker = nums[i];
-            int left = 0, right = piles;
-            while (left < right) {
-                int mid = left + (right - left) / 2;
-                if (top[mid] > poker) {
-                    right = mid;
-                } else if (top[mid] < poker) {
-                    left = mid + 1;
-                } else {
-                    right = mid;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-            if (left == piles) piles++;
-            top[left] = poker;
         }
-        return piles;
+        int result = 0;
+        for (int i = 0; i < dp.length; i++) {
+            result = Math.max(result, dp[i]);
+        }
+        return result;
     }
 }
 // @lc code=end
