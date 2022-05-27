220daf26666c17a713334576ee10492b2841bc8a
Haoyang Chen <haoyangchen0901@gmail.com>
2022-05-27 12:36:57 -0700

    Complete Question 322 with Bottom Up Approach

--- 322.coin-change.java
+++ 322.coin-change.java
@@ -4,6 +4,8 @@
  * [322] Coin Change
  */
 
+ import java.util.*;
+
 // @lc code=start
 // 
 // Method: Dynamic Programming - Top Down
@@ -35,27 +37,48 @@
 
 
 // Method 2: Top Down Approach
-class Solution {
-    private int[] memo;
-    public int coinChange(int[] coins, int amount) {
-        memo = new int[amount + 1];
-        Arrays.fill(memo, -666);
-        return dp(coins, amount);
-    }
+// class Solution {
+//     private int[] memo;
+//     public int coinChange(int[] coins, int amount) {
+//         memo = new int[amount + 1];
+//         Arrays.fill(memo, -666);
+//         return dp(coins, amount);
+//     }
     
-    private int dp(int[] coins, int amount) {
-        if (amount == 0) return 0;
-        if (amount < 0) return -1;
-        if (memo[amount] != -666) return memo[amount];
+//     private int dp(int[] coins, int amount) {
+//         if (amount == 0) return 0;
+//         if (amount < 0) return -1;
+//         if (memo[amount] != -666) return memo[amount];
         
-        int result = Integer.MAX_VALUE;
-        for (int coin: coins) {
-            int subProblem = dp(coins, amount - coin);
-            if (subProblem == -1) continue;
-            result = Math.min(result, subProblem + 1);
+//         int result = Integer.MAX_VALUE;
+//         for (int coin: coins) {
+//             int subProblem = dp(coins, amount - coin);
+//             if (subProblem == -1) continue;
+//             result = Math.min(result, subProblem + 1);
+//         }
+//         memo[amount] = (result == Integer.MAX_VALUE) ? -1 : result;
+//         return memo[amount];
+//     }
+// }
+
+
+// Method 3: Bottom Up Approach
+// Time Complexity: O(S * n) where n is the number of denominations of the coins
+// Space Complexity: O(S) where S is the amount
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int maxValue = amount + 1;
+        int[] dp = new int[maxValue];
+        Arrays.fill(dp, maxValue);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
         }
-        memo[amount] = (result == Integer.MAX_VALUE) ? -1 : result;
-        return memo[amount];
+        return dp[amount] == maxValue ? -1 :dp[amount];
     }
 }
 // @lc code=end
