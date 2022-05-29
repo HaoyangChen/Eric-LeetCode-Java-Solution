fd4a238f9c2f4f355bb2effdf0652b3b8801ed15
Haoyang Chen <haoyangchen0901@gmail.com>
2022-05-29 15:41:20 -0700

    Add Optimized DP Approach to Question 518

--- 518.coin-change-2.java
+++ 518.coin-change-2.java
@@ -5,24 +5,42 @@
  */
 
 // @lc code=start
+// class Solution {
+//     public int change(int amount, int[] coins) {
+//         int n = coins.length;
+//         int[][] dp = new int[n + 1][amount + 1];
+//         for (int i = 0; i <= n; i++) {
+//             dp[i][0] = 1;
+//         }
+        
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= amount; j++) {
+//                 if (j - coins[i - 1] >= 0) {
+//                     dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+//                 } else {
+//                     dp[i][j] = dp[i - 1][j];
+//                 }
+//             }
+//         }
+//         return dp[n][amount];
+//     }
+// }
+
+
+// optimized dp
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n + 1][amount + 1];
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 1;
-        }
-        
-        for (int i = 1; i <= n; i++) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int i = 0; i < n; i++) {
             for (int j = 1; j <= amount; j++) {
-                if (j - coins[i - 1] >= 0) {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
+                if (j - coins[i] >= 0) {
+                    dp[j] = dp[j] + dp[j - coins[i]];
                 }
             }
         }
-        return dp[n][amount];
+        return dp[amount];
     }
 }
 // @lc code=end
