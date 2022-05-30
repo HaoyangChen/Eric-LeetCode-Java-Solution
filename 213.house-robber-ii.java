07ae5840f47ce0c6e9f0c2f963170b773e89615d
Haoyang Chen <haoyangchen0901@gmail.com>
2022-05-30 12:16:04 -0700

    Complete Question 213 with DP

--- 213.house-robber-ii.java
+++ 213.house-robber-ii.java
@@ -7,7 +7,19 @@
 // @lc code=start
 class Solution {
     public int rob(int[] nums) {
-        
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        return Math.max(robRange(nums, 0, n - 2), robRange(nums, 1, n -1));
+    }
+
+    private int robRange(int[] nums, int start, int end) {
+        int dpNext = 0, dpNextNext = 0, dpCurrent = 0;
+        for (int i = end; i >= start; i--) {
+            dpCurrent = Math.max(dpNext, nums[i] + dpNextNext);
+            dpNextNext = dpNext;
+            dpNext = dpCurrent; 
+        }
+        return dpCurrent;
     }
 }
 // @lc code=end
