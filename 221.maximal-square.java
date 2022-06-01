bffed1942f631056e7affae4fa88a1c3e885e0fa
Haoyang Chen <haoyangchen0901@gmail.com>
2022-06-01 10:41:31 -0700

    Add Additional DP Implementation to Question 221

--- 221.maximal-square.java
+++ 221.maximal-square.java
@@ -24,6 +24,33 @@
 //     }
 // }
 
+// class Solution {
+//     public int maximalSquare(char[][] matrix) {
+//         int m = matrix.length, n = matrix[0].length;
+//         int[][] dp = new int[m][n];
+//         for (int i = 0; i < m; i++) {
+//             dp[i][0] = matrix[i][0] - '0';
+//         }
+//         for (int j = 0; j < n; j++) {
+//             dp[0][j] = matrix[0][j] - '0';
+//         }
+        
+//         for (int i = 1; i < m; i++) {
+//             for (int j = 1; j < n; j++) {
+//                 if (matrix[i][j] == '0') continue;
+//                 dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i - 1][j - 1], dp[i][j - 1])) + 1;
+//             }
+//         }
+//         int length = 0;
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 length = Math.max(length, dp[i][j]);
+//             }
+//         }
+//         return length * length;
+//     }
+// }
+
 // Approach 2: Better Dynamic Programming
 public class Solution {
     public int maximalSquare(char[][] matrix) {
