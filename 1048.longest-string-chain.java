4f2c9eaec824ced5755c3cf512268ddb5e344194
Haoyang Chen <haoyangchen0901@gmail.com>
2022-06-02 23:49:13 -0700

    Add Question 1048

---
+++ 1048.longest-string-chain.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1048 lang=java
+ *
+ * [1048] Longest String Chain
+ */
+
+// @lc code=start
+class Solution {
+    public int longestStrChain(String[] words) {
+        
+    }
+}
+// @lc code=end
+
