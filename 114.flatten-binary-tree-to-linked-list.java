a72b12416b20d3773f55569aa45ea07a2eac53fb
Haoyang Chen <haoyangchen0901@gmail.com>
2022-06-25 12:13:05 -0700

    Add Additional Implementation to Question 114

--- 114.flatten-binary-tree-to-linked-list.java
+++ 114.flatten-binary-tree-to-linked-list.java
@@ -43,22 +43,45 @@
 // O(1) space iterative solution
 // Time Complexity: O(N) since we process each node of the tree at most twice
 // Spcae Complexity: O(1)
+// class Solution {
+//     public void flatten(TreeNode root) {
+//         if (root == null) return;
+//         TreeNode node = root;
+//         while (node != null) {
+//             if (node.left != null) {
+//                 TreeNode rightMost = node.left;
+//                 while (rightMost.right != null) {
+//                     rightMost = rightMost.right;
+//                 }
+//                 rightMost.right = node.right;
+//                 node.right = node.left;
+//                 node.left = null;
+//             }
+//             node = node.right;
+//         }
+//     }
+// }
+
+
 class Solution {
-    public void flatten(TreeNode root) {
-        if (root == null) return;
-        TreeNode node = root;
-        while (node != null) {
-            if (node.left != null) {
-                TreeNode rightMost = node.left;
-                while (rightMost.right != null) {
-                    rightMost = rightMost.right;
-                }
-                rightMost.right = node.right;
-                node.right = node.left;
-                node.left = null;
-            }
-            node = node.right;
-        }
+    HashMap<Integer, Integer> valueToIndex = new HashMap<>();
+    
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            valueToIndex.put(inorder[i], i);
+        }    
+        return build(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+    }
+    
+    private TreeNode build(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd) {
+        if (inStart > inEnd) return null;
+        int rootval = postorder[postEnd];
+        int index = valueToIndex.get(rootval);
+        int leftSize = index - inStart;
+        TreeNode root = new TreeNode(rootval);
+        root.left = build(inorder, inStart, index - 1, postorder, postStart, postStart + leftSize - 1);
+        root.right = build(inorder, index + 1, inEnd, postorder, postStart + leftSize, postEnd - 1);
+        return root;
     }
 }
 // @lc code=end
